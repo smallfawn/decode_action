ec55a4c2d959a63a8466942b0d13ab48406e34e2
action <action@github.com>
2024-06-28 00:19:52 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,6 @@
-function hi() {
-  console.log("Hello World!");
-}
-hi();
+(function (_0x24aa09, _0x2cb0eb) {
+  _0x24aa09.update = "2019年09月07日15:24:29更新";
+  _0x2cb0eb.info = "这个是一个本站对JavaScript 脚本的一个高级加密，兼容性较强";
+  _0x2cb0eb.feedback = "有问题请联系QQ 84034666";
+})(window, document);
+_0xodA = "jsjiami.com.v6";
