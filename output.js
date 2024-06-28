1803c57853b7a61124745efcc5b079cb6c6c9096
action <action@github.com>
2024-06-28 07:36:24 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Fri Jun 28 2024 07:33:46 GMT+0000 (Coordinated Universal Time)
+//Fri Jun 28 2024 07:36:24 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("顺丰速运"),
