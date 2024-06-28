13379b1b8c9fe50e776a5546db80b36c67144e4a
sdmpy123 <81862130+sdmpy123@users.noreply.github.com>
2024-06-29 00:32:31 +0800

    得间

--- input.py
+++ input.py
@@ -13,7 +13,7 @@ new Env("得间小说")
 安装pycryptodome依赖
 必须抓开宝箱的全部链接https://dj.palmestore.com/zycl/gold/receiveV5?1234567
 搜索param=50,即可找到宝箱的链接
-变量：djxs，多号====分开,不能用青龙并发
+变量：djxs，多号===分开,不能用青龙并发
 格式：备注#链接
 一天一次即可，宝箱没什么金币（宝箱视频一次性跑完）
 新增pushplush表格推送，一目了然
