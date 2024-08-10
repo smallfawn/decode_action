526a7d737a6b6fe2c516eb4787543776a205020d
action <action@github.com>
2024-08-10 03:20:06 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Fri Aug 09 2024 16:11:31 GMT+0000 (Coordinated Universal Time)
+//Sat Aug 10 2024 03:20:06 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const _0x59a330 = new _0xc5c85b("七猫免费小说");
