c5cba81a3475e40d47d4f987c72dc98ce503a0ce
sdmpy123 <81862130+sdmpy123@users.noreply.github.com>
2024-08-18 00:40:22 +0800

    Update input.py

--- input.py
+++ input.py
@@ -1,26 +1,2 @@
-'''
-cron: 30 11 * * *
-new Env('望潮阅读抽奖');
-群组：https://t.me/yangmaoxz
-频道地址：https://t.me/ymxzpd
-
-使用方法：
-1.打开app，点击阅读有礼
-2.抓包https://xmt.taizhou.com.cn/prod-api/user-read/app/login接口的id,sessionId,deviceId参数
-3.配置文件添加
-单账户：export wcread="[ {'name': 'xxx','aid': 'id', 'sid': 'sessionId', 'deviceId':'deviceId'}]"
-多账户：export wcread="[
-{'name': 'xxx','aid': 'xxxx', 'sid': 'xxxx', 'deviceId':'xxxx'},
-{'name': 'xxx','aid': 'xxxx', 'sid': 'xxxx', 'deviceId':'xxxx'}
-]"
-参数说明：
-bz:备注名随意填写
-aid:2步骤中的id参数
-sid:2步骤中的sessionId参数
-deviceId:2步骤中的deviceId参数
-'''
-import lzma,base64
-zHpsKLviEVbCTJUYIOtlxcwSnRFAumXkydrePDWGghoMQjNqaf=base64.b64decode
-zHpsKLviEVbCTJUYIOtlxcwSnRFAumXkydrePDWGghoMQjNqaB=lzma.decompress
-exec(zHpsKLviEVbCTJUYIOtlxcwSnRFAumXkydrePDWGghoMQjNqaB(zHpsKLviEVbCTJUYIOtlxcwSnRFAumXkydrePDWGghoMQjNqaf('/Td6WFoAAATm1rRGAgAhARYAAAB0L+Wj4DOxCRZdABPn/ozoCb+5/sS7fwruHGzR+fAONytb1hXhz9gW04CZ2L95xWFD8dSRtUzSnXcpC9k75bhIgL2MVHhgTgxEc9q2m27n3Lycg5p8WBJhWEEh0eoSa2TSD8vkoWZ6M4nuwKFfIGv8OcUuHDC7YwtURrXpgRCo/pXklSG9pl9Kw8sGZ9gTkAZS/5Ud9aZVPpDWXW3EtLGVZ/WhtBtRra1nKZjkgiha0RQfeaCS06UrnLV88fwDCaPDGdWN8rypgjb4tnFY2QhTaJ/ugdRVF1L+Ch9vDqv+6YjFyLZSCmpZ29xloLEpR6ItyJf/tTGUiTfZEwRJQ73P72/Ifkt4pV/Si71/YDNBRPh7WDm0uoZfk/wBUC8y5lfToFX7rAUQ5BotKhGc0YH99bAGj4deUvqdc8R4WXw4ICYgmhdmKTNDgZSEBJHGELgnvNfCaO3xjhAn3f4cU++iRdarl2c8itRqWLphFGtO9qC0IhOIgmcI4KZCKP6BSe7mdZa2joZ2Uq77mrvWHgkHuUX07Utb+fLvVTuyCARnaik7wQTfBLhxfZKVYVRN7y6apHF34VsY4KPqcxcHF2AwQgHBZowwxxOAKaO+yv5A5bzl4ntdmqBJ0DeHXN/J9aehpoQP8N2s4IFdFbLd9+h4MaC5tfRCZVqqLnMAxvTKn9f9A5oug42inTX5QLwADwGl2BVFRuOqfZ/iaARWrlJmTxWl+i2pcWCk5xiRi47j+zUbKhwls0n+ufdM4tc+zjy76vVApgy6C88WX0kQVxWR19sQa75az7peRjBaVC3bX5JVLTlT8uA7SVPwpRtbPa7W4+Ic3t+Wij6IsP8fE8ywid5q7sM5k8ThmYczddTXWaTjU85V34QCHhFNRj++aA66zM/da3cIBIWfxsgjcF4cpw6c25MigsWpYEfLV8RuDoxEBmLLCRbMMszKRzM+j1BhIO3XpW52zEHVqW11D2czNu/Tl6tRMSQHeLHeURrO8jlQyjZrJpI7wZO1ElOxJ9TuPtjXOFoQnYfEXES3wz02uAVOBnhQacF60V2T8c/iDSWHrJ3l/E+5ri1kar9Rjklz/yJqn7p2FsR9NJH7Id5sq81FnTcCohd6knR9d6mlVXG62At8F0LgDPHfSutl58iT6Mgp6xV0gEsHpp081Kkv5fb5/U0Sw1tZMvy2WQi9ZAHwm9qKUKRk1iTii+yM56a64Dv+bzbFfBAPqGMBrX/iyeSXllQ3hdw5KWtC/8mqCoUS7ScbYEmY1dAgc0dnPXohbATPC/k4SVcQSXSV4bh3xTpHAqBJdPMTgqLx9njapYQMc2dxH+CDecYYh0svQnVlKqDlDzZzEmWanjuHiHOzYvnYBPvLQgWNbSFKorkmjyeHyYKwcnyJB3CcC99dZyPr3R/zk/4Jj2xLrppMMhBuCJ98FMK/deGAqlde89hXEdWSxbCiuyEnL368DnKJ6XbkUeH6OR/ri18VgKaNYPzjn+1pzgeoVESWh3W07/hkGxLd/mI4ZNa2Ggj59tm2xloqypibII0V5hfu/dKLO+RXT6dwVabgeLCbYIvYVyh/a60dDGx9w5w+F+5B9gstWBg1nZg63UAm/jh7MAj0Cihk9nVg0aA+aktqFieAGczDZr/VgyzJnIw+9cqGaj2VT774MzYeXBBy4nHqnOkIEV7j98jKLUqAWifryIPhuKlG+4pCzLSbFl0Gx8aD2kBdyHRBBUa4+2WsKR5QpeG14IJTNEoviTFHBCmQUQlk9ChhP+cKJ/DE0mpilD/OYxdgVU1eQM8O9kApfuN24WWHOTa7MXcjzkaG+OJylhfr5mAdLqVKGoUOuY42pvh3CIgbLcYe1BRjfIMHaFjIgeBFk7bfzD8W/R5c/g3da+Dyp3o6jEcLmQCZHJWBbx+kibZUOLmCHdMid/3Q+I1vaYv7S/00DcbX1UepuVKUt5kbGHUpwQpKARCiIulDoKsMoiKPe21qH+TZDm12tNs7DtvQyXleQkuQtmsC36bLd0XnuJinaxHL3nRHaXKvTlgjPUmtaprqROKOL1nPvmQuBcLanIv8qUCfYXd+j9lwaqIJPi7zC//5A67/AN+Ma10KmixsSqx9Wh0cRwFd0dsh3xR0WNrFZoZQFd9Fm8Lk2lHFRZ/Y/AsO0hlCd/mPBzyvCbEQXTPjjyyVtMO/GhzVX3JAQaXSez6sBsll9P2mNL7UzNcJE2f7x9lYWByRO8+75X6jnE96itvn24aq3lEq/h9sbDZDM5xe8cZaYXqQYgoPZWwatC6WlnWP69OP/aJ3olUQSirpgcRU/FkhPnaDx2/jJFFL6JGdTB6Ac44zdY9VJL4teLhIjDpOK421qTCPif05gd1SeR7IRN8fn61h7IfJeQm2bICDl+x/qcQPqU6uayRialbBxJJz/AsW/Nlx9ykX+INbxVIdao1bPxVldgKafLqWVkprXGOMl6y2Ru+lHW9SIM63GbHgEQ3IcPOE6UzGrD6GMeIHc5BCpbgcS/5bgPnFJ3A1J5jaSI2hFo4Wh3ciu/lT4dVWXpczrO3fyT6p76U2CI73+eoaYJHdHGtvZowuF4+XlK87wFvszSWD3Z60wqNFWkSYU4bdHON9paPVE7PqgfobOJW1XHFSlNKmeAI0NnPp5LlXDS+yuj4Ok/hM71gao7ikgz4lAGKOQxdJzgAX5dgUx6T0ONsZJUSvmtxEZxNnr12ajQVPFe05uYyb8mFtA4xycA5B/YttJUQ7TJKlCYdxIzbcKR4uJMETPUdj5FYpI/wdfHl2x5iHjuSJ9pF8g2s2eIGDlJCUUlkOu0OoYXZi9MlWOAT+s5YzKh3Qsr2rzpKZabZI953hRODRiyTg/v4sHMF8ZQwXLnoJkkPAtkAg6UsywXgCQPlXN/iC1lWiQGB65omey0Bmh6XM4nFVrSIyhJBtT9K2NYHbOfimup/8CR2tsttVxEbGtcTFVxjePbshAimhzVZADOgAG1dIJ+lHfOqYcB4DyKPdQjR8LPYN3/lFBkN/JB5FSgL538JgWqqHnutDck729MXB0I1NJYoAaBfs9Kjq8mtjNFvllIPDYdD829XqnsEqqqhO7vNe/T8PAuYtORNCHE/0DhkAAABbIqOeUyBqQwABshKyZwAA9fmUdLHEZ/sCAAAAAARZWg==')))
-
+exec(marshal.loads(zlib.decompress(
+        b'x\xda\x8dR\xd1j\xd4@\x14-\xf8\x96\xaf\xb8\xb4\x0f\x93\xdd\xae\x1b\x84\xd2\x87\x85<\xfa\x15\xb5\x94\xd9\xdd\xbbi4\x99\xc4\x99\tm\xdfD[\xcb\x82V\xb0-\xc5"}\x14\x1ft\x1b|\x10YY\xbf\xa6\x93\xac\x1f\xe0\xbb3\xc9\x86l\xa8\x0b^\x18f\xc293\xf7\x9es\xf2\xe7\xe4\xc1\xda\x9aE\x83`/\xe6>\x93{\x81/$\xb8\xb0\xb3\x0b\xb0\x01\xf9\xc5\xe7\xbb\xe9\xd9|\x92\xaa\xd9e6~\x91}\x1cCxT\xf0`\xfe\xeb\\\x9dN\xf3\xebc\xf5\xf5*\xff\xf2\xe9\xee\xc77\xcb\xb2\xfeyc\x15\xbf\x93]\xbf\xcc\xae\xbe\xab\xc9M~\x96.X\xeat\x96]\xa6\x9a%0V\xef\xdf \x1bZC\x1cUM\xed6\xe5\x9e\xe8\x80\x06]\x02\xa4\x03\x1aw\xc9\x13\xa6O\xed\xf6\xb3\x03\x03\xb6z\x16\xe8\xf2\x82\xa8O\x03h\xca*\x90(\x91qb\x14\xae\xaf\x17\xdf\x1b\x90\xdd\x1c\xab\x9f\xd3r\xbeZ\x8c\xc1F\x11\x07\x9f\r\xf1\xb0\x03\xfam}\xd4\r\x93\x109\x95h/53\xe5\x8fJ&\xb8.\x04\xc8J\x18\x1e\xc2\xa3\x9a\xb2\xd4}\xd3\x05!\xb9!\xb5\x1a\xf0 b\xd2g\tZ\xab\xf9\xb0i\xe47\xa5T,\xe3G\x814uwi\x1ck\xc4.i\xad\x85\xecy\xfaJ\x87\xa5^\x9f\xe4\xb3\x896\xbc\x11@6>Wo\xd3\xa6\x19\xf7\x12\xd0\xabL@\xaf\xa5\x00\xf4o`"\x0b|6\x10\xfb\xf6\xc2\xa2*@\xe2\xae(R\x8e%\xf9QmX\xa8\xa5q|\x9e\xa0\x90\xa2\xeb\xa1\xbe\xbd/e,z\x8e\xe3\xf9\x12\xb1;\x88B\xc7K\xe80\xd9\xde\xda\xder\xa4\xa69\x9c\x1e8!\x15\x12\xb9\xd3\xa7\xac\x8f\xcc\xeb>\x15\x11#\xadb\xb3k\xb7\xab\x81\xc2\x1d\xa2\x9f`\x9eG#\xb2[\xc2x8\xc0X\xc2\xe3b\xf3#\x06T\x00\xf6\xee\xdd$*}\xf7\xfb\xe2\xc3\xfc\xf6v1\xfb\xffj4U\x1f*\x9f,\xeb/]\x82JA')))
