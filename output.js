04306552e24c6f8256f7ee8603ce38b405097cbf
action <action@github.com>
2024-10-18 04:35:48 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,19 +1,35 @@
-//Fri Oct 18 2024 03:22:03 GMT+0000 (Coordinated Universal Time)
+//Fri Oct 18 2024 04:35:48 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
-(function () {})();
+function generateRandom16DigitNumber() {
+  let _0x2fc4f3 = "";
+  for (let _0x10ce2b = 0; _0x10ce2b < 8; _0x10ce2b++) {
+    _0x2fc4f3 += Math.floor(Math.random() * 10).toString();
+  }
+  return _0x2fc4f3;
+}
 if ($request.method.toUpperCase() !== "OPTIONS") {
-  let body = $request.body,
-    _body = JSON.parse(body),
-    headers = $request.headers,
-    url = $request.url;
-  _body.market && (_body.market = "NG");
-  _body.locale && (_body.locale = "en-NG");
-  _body.friendlyName && (url.includes("appId=storeCart") ? _body.friendlyName = "cart-save-for-later-NG" : _body.friendlyName = "cart-NG");
-  headers["X-MS-Market"] && (headers["X-MS-Market"] = "NG");
-  headers["x-ms-market"] && (headers["x-ms-market"] = "NG");
-  $done({
-    "headers": headers,
-    "body": JSON.stringify(_body)
+  let deviceid = $persistentStore.read("deviceid");
+  !deviceid && (deviceid = generateRandom16DigitNumber(), $persistentStore.write(deviceid, "deviceid"));
+  let url = "http://verify.idamie.com/family.php?id=" + deviceid;
+  $httpClient.get(url, function (_0x3773c5, _0x44103a, _0x41186e) {
+    if (_0x3773c5) $notification.post("XBOX", "设备验证失败", "您的设备id: " + deviceid), $done({});else {
+      if (_0x41186e === "error") $notification.post("XBOX", "设备验证失败", "您的设备id: " + deviceid), $done({
+        "$response": $response
+      });else {
+        if (_0x41186e === "true") {
+          let _0x9ba736 = $response.body,
+            _0x22ddec = JSON.parse(_0x9ba736);
+          if (_0x22ddec.cart && _0x22ddec.cart.id) {
+            let _0xac7044 = _0x22ddec.cart.id;
+            $persistentStore.write(_0xac7044, "cartId");
+            $notification.post("XBOX", "执行成功", "");
+          } else $notification.post("XBOX", "执行失败", "");
+          $done({
+            "body": $response.body
+          });
+        } else $notification.post("XBOX", "设备验证失败", "您的设备id: " + deviceid), $done({});
+      }
+    }
   });
 } else $done({});
