c61dfbadd124a3415e77313bbaee97116bbd6388
action <action@github.com>
2024-12-08 05:24:11 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Sun Dec 08 2024 05:23:42 GMT+0000 (Coordinated Universal Time)
+//Sun Dec 08 2024 05:24:11 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("益禾堂");
