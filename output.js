9109920d5155d503800442e381bd9140ab1eb9f5
action <action@github.com>
2025-01-02 09:26:20 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Thu Jan 02 2025 09:21:39 GMT+0000 (Coordinated Universal Time)
+//Thu Jan 02 2025 09:26:20 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const _0x49dfef = _0x5370a4("电信营业厅"),
