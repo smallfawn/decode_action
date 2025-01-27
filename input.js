9003ab039b5a3b751d65c684f36b6a36633d598b
345678ertyu <165012554+345678ertyu@users.noreply.github.com>
2025-01-27 23:21:55 +0800

    Update input.js

--- input.js
+++ input.js
@@ -1,25 +1,4 @@
-//Mon Jan 27 2025 15:16:54 GMT+0000 (Coordinated Universal Time)
-//Base:https://github.com/echo094/decode-js
-//Modify:https://github.com/smallfawn/decode_action
-/*
-中国联通 v2.08
 
-包含以下功能:
-阅光宝盒, 首页签到, 立减金打卡, 阅读专区抽奖, 云盘抽奖, 联通祝福
-支付日活动 (每个月26-28号)
-618集卡活动 (5.31-6.20)
-
-首页签到默认运行, 需要关闭的设置变量 chinaUnicomSign='false'
-联通祝福默认运行, 需要关闭的设置变量 chinaUnicomLtzf='false'
-
-定时每天两三次
-需要在联通APP中选择退出登录-切换账号登录, 捉下面这个包
-https://m.client.10010.com/mobileService/onLine.htm
-把请求体(body)里面的token_online参数填到变量 chinaUnicomCookie 里, 多账号换行或&或@隔开:
-export chinaUnicomCookie="a3e4c1ff25da2xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-不会捉包的可以使用网页获取: https://service.leafxxx.win/unicom/login.html
-
-cron: 0 0,7,20 * * *
 const $ = new Env("中国联通");
 */
 function _0x2abc(_0x2dbfcb, _0x396f1d) {
