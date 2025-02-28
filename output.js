951c154ea81010b19dce7987755d3d490174cc32
action <action@github.com>
2025-02-28 09:47:09 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,89 +1,94 @@
-//Fri Feb 28 2025 09:45:16 GMT+0000 (Coordinated Universal Time)
+//Fri Feb 28 2025 09:47:09 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const crypto = require("crypto");
-function MD5(_0x3582b8) {
-  return crypto.createHash("MD5").update(_0x3582b8).digest("hex");
+function MD5($OQQ$Q) {
+  return crypto.createHash("MD5").update($OQQ$Q).digest("hex");
 }
-function getSign(_0x3d3a73) {
-  let _0x174a2b = MD5(_0x3d3a73);
-  let _0x285f90 = _0x174a2b.substr(0, 1);
-  let _0x5d6c68 = _0x174a2b.substr(-1, 1);
-  let _0x155e5a = _0x285f90 + _0x5d6c68;
-  return MD5(_0x3d3a73 + _0x155e5a);
+function getSign(OO00OQ0) {
+  let $QQO0Q = MD5(OO00OQ0);
+  let O$$Q = $QQO0Q.substr(0, 1);
+  let QOQOOQO = $QQO0Q.substr(-1, 1);
+  let Q0QQO0Q = O$$Q + QOQOOQO;
+  return MD5(OO00OQ0 + Q0QQO0Q);
 }
-async function getSig56(_0x4dd35d = {}, _0x314ec6 = null, _0x1fefd6 = "get", _0x36824c = "json", _0x275ba8) {
+async function getSig56(O0OQQO = {}, OOQ$ = null, $$0 = "get", $O0QQ = "json", OO0$$0) {
   if (!process.env.kuaishou_speed_openbox || !process.env.kuaishou_speed) {
     return "";
   }
-  const _0x44ccc3 = require("path");
-  if (!(_0x44ccc3.resolve(__dirname).indexOf("smallfawn") !== -1)) {
+  const O000OOO = require("path");
+  if (!(O000OOO.resolve(__dirname).indexOf("smallfawn") !== -1)) {
     return "";
   }
-  let _0x350e87 = "http://yi100.top:5666/sig56";
+  let O0QQO$ = "http://yi100.top:5666/sig56";
   try {
     {
-      const _0xe5a85c = {
-        Cookie: _0x275ba8
+      const O0OQQQ = {
+        "Cookie": OO0$$0
       };
-      const _0x47c647 = {
-        query: _0x4dd35d,
-        data: _0x314ec6,
-        method: _0x1fefd6,
-        type: _0x36824c
+      const OO0$QQ = {
+        "query": O0OQQO,
+        "data": OOQ$,
+        "method": $$0,
+        "type": $O0QQ
       };
-      let _0x31f90e = {
-        url: _0x350e87,
-        headers: _0xe5a85c,
-        method: "POST",
-        data: _0x47c647
+      let OQO$$ = {
+        "url": O0QQO$,
+        "headers": O0OQQQ,
+        "method": "POST",
+        "data": OO0$QQ
       };
-      _0x31f90e.headers.sign = getSign(JSON.stringify(_0x31f90e.data));
+      OQO$$.headers.sign = getSign(JSON.stringify(OQO$$.data));
       let {
         data: _0x3f9437
-      } = await axios.request(_0x31f90e);
+      } = await axios.request(OQO$$);
       return _0x3f9437 ? _0x3f9437.s3 ? _0x3f9437.s3 : false : false;
     }
-  } catch (_0x2bd529) {
+  } catch (QOOO000) {
     {
-      return false;
+      {
+        return false;
+      }
     }
   }
 }
-async function getSig68(_0x3d319d = {}, _0x2c2fd1 = null, _0x5a57ad = "get", _0x500dc9 = "json", _0x46a3af) {
+async function getSig68(OOQQQ0Q = {}, Q$$0$O = null, O0O00OQ = "get", $$$OQQ = "json", $0O$O$) {
   if (!process.env.kuaishou_speed_openbox || !process.env.kuaishou_speed) {
+    {
+      return "";
+    }
+  }
+  const O00OQ00 = require("path");
+  if (!(O00OQ00.resolve(__dirname).indexOf("smallfawn") !== -1)) {
     return "";
   }
-  const _0x4e4bff = require("path");
-  if (!(_0x4e4bff.resolve(__dirname).indexOf("smallfawn") !== -1)) {
-    return "";
-  }
-  let _0x48f0f4 = "http://yi100.top:5666/sig68";
+  let QO$QQ = "http://yi100.top:5666/sig68";
   try {
     {
-      const _0x4f1802 = {
-        Cookie: _0x46a3af,
-        sign: ""
+      const OOOQ00O = {
+        "Cookie": $0O$O$,
+        "sign": ""
       };
-      const _0xf0dcbf = {
-        query: _0x3d319d,
-        data: _0x2c2fd1,
-        method: _0x5a57ad,
-        type: _0x500dc9
+      const Q0OOO0O = {
+        "query": OOQQQ0Q,
+        "data": Q$$0$O,
+        "method": O0O00OQ,
+        "type": $$$OQQ
       };
-      let _0x48121d = {
-        url: _0x48f0f4,
-        headers: _0x4f1802,
-        method: "POST",
-        data: _0xf0dcbf
+      let $$O$Q0 = {
+        "url": QO$QQ,
+        "headers": OOOQ00O,
+        "method": "POST",
+        "data": Q0OOO0O
       };
-      _0x48121d.headers.sign = getSign(JSON.stringify(_0x48121d.data));
+      $$O$Q0.headers.sign = getSign(JSON.stringify($$O$Q0.data));
       let {
         data: _0x3276cb
-      } = await axios.request(_0x48121d);
+      } = await axios.request($$O$Q0);
       return _0x3276cb ? _0x3276cb.s3 ? _0x3276cb.s3 : false : false;
     }
-  } catch (_0x2fe28a) {
+  } catch (OOQOO0O) {
     return false;
   }
 }
+OＯ0$ = "jsjiami.com.v6";
