6bca9b711fb3147941e3658e20429d4a515fec02
action <action@github.com>
2025-03-20 01:46:11 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Fri Mar 07 2025 11:34:55 GMT+0000 (Coordinated Universal Time)
+//Thu Mar 20 2025 01:46:11 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("巴奴火锅");
