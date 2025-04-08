3ad2e98a46343bd703c39913584f2f2a88979646
action <action@github.com>
2025-04-08 08:20:03 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,92 +1,12 @@
-//Tue Apr 08 2025 08:18:51 GMT+0000 (Coordinated Universal Time)
+//Tue Apr 08 2025 08:20:03 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-function ff() {
-  var _0x142e43 = 0,
-    _0x5dbc4a = 2,
-    _0x276195 = _0x5dbc4a * 95;
-  if (_0x142e43 % 1 == 0) {
-    {
-      var _0x5ed839 = "bdcse".toString().substr(0, 1),
-        _0x172406 = "bdcse".toString().substr(2, 2),
-        _0x5b746a = _0x172406 + "" + _0x5ed839,
-        _0x367982 = md5(_0x5b746a);
-      return _0x367982;
-    }
-  } else {
-    var _0x367982 = md5(_0x142e43);
-    return _0x367982;
-  }
-}
-function aa() {
-  var _0x20f51f = 10;
-  if (_0x20f51f >= 10) {
-    var _0x344743 = _0x20f51f.toString().substr(0, 1);
-    var _0x468a92 = _0x20f51f.toString().substr(1, 2);
-    var _0x3fcb3c = _0x468a92 + "" + _0x344743;
-    var _0x509597 = parseInt(_0x3fcb3c);
-    var _0x5a06f3 = _0x509597 * 80;
-    var _0x37c659 = _0x5a06f3 + _0x26dcaf;
-    var _0x42ed5f = md5(_0x37c659);
-    return _0x42ed5f;
-  }
-  var _0x26dcaf = 0;
-  var _0x20f51f = 0;
-  var _0x509597 = 2;
-  var _0x5a06f3 = _0x509597 * 95;
-  if (_0x20f51f % 1 == 0) {
-    var _0x344743 = "bdcse".toString().substr(0, 1);
-    var _0x468a92 = "bdcse".toString().substr(2, 2);
-    var _0x3fcb3c = _0x468a92 + "" + _0x344743;
-    var _0x27cee0 = md5(_0x3fcb3c);
-    return _0x27cee0;
-  } else {
-    var _0x27cee0 = md5(_0x20f51f);
-    return _0x27cee0;
-  }
-}
-function ee() {
-  var _0x425367 = sessionStorage.getItem("ffb"),
-    _0x18c90d = sessionStorage.getItem("ss");
-  if (_0x425367 >= 10) {
-    var _0x26d43f = _0x425367.toString().substr(0, 1);
-    var _0x531469 = _0x425367.toString().substr(1, 2);
-    var _0x4bac92 = _0x531469 + "" + _0x26d43f;
-    var _0x15c7b7 = parseInt(_0x4bac92);
-    var _0x1f8bc0 = _0x15c7b7 * 90;
-    var _0x4abc8a = _0x1f8bc0 + _0x18c90d;
-    var _0x3b4912 = md5(_0x4abc8a);
-    return _0x3b4912;
-  } else {
-    var _0x155bca = _0x425367 * 90,
-      _0x4b854b = _0x155bca + _0x18c90d,
-      _0x3b4912 = md5(_0x4b854b);
-    return _0x3b4912;
-  }
-}
-function bb() {
-  var _0x2d9eaf = "ffb";
-  if (_0x2d9eaf.length >= 2) {
-    var _0x184a4e = _0x2d9eaf.toString().substr(0, 1);
-    var _0x544bf0 = _0x2d9eaf.toString().substr(2, 2);
-    var _0x284a96 = _0x544bf0 + "" + _0x184a4e;
-    var _0x595e80 = md5(_0x284a96);
-    return _0x595e80;
-  } else {
-    var _0x595e80 = md5(_0x2d9eaf);
-    return _0x595e80;
-  }
-}
-function dd() {
-  var _0x1943d0 = 0;
-  if (_0x1943d0 % 1 == 0) {
-    var _0x1f7ec8 = "bdcse".toString().substr(0, 1);
-    var _0x220f24 = "bdcse".toString().substr(2, 2);
-    var _0x4279a9 = _0x220f24 + "" + _0x1f7ec8;
-    var _0x2669e7 = md5(_0x4279a9);
-    return _0x2669e7;
-  } else {
-    var _0x2669e7 = md5(_0x1943d0);
-    return _0x2669e7;
-  }
+function lj() {
+  var _0xb5313b = sessionStorage.getItem(TOKEN);
+  var _0x3712f8 = sessionStorage.getItem(START_NUM);
+  var _0x4037fd = sessionStorage.getItem(STR);
+  var _0x3c914a = ee();
+  _0xb5313b = _0xb5313b + "&startNumber=" + _0x3712f8 + "&str=" + _0x4037fd + "&keys=" + _0x3c914a + "&ds=";
+  var _0x554c2f = "/uaction/edbbcdc/getArticleListkk?token=" + _0xb5313b;
+  return _0x554c2f;
 }
