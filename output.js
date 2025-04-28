78cae7fcda8a9296408ea449a639e53bd4edd8cb
action <action@github.com>
2025-04-28 10:50:05 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,80 +1,21 @@
-//Mon Apr 28 2025 10:49:23 GMT+0000 (Coordinated Universal Time)
+//Mon Apr 28 2025 10:50:05 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-if (key1 == "8366391277") {
-  getMusic();
-  var _0x19b422 = http.request(url, {
-    "method": "PUT",
-    "headers": {
-      "Authorization": "Basic " + code,
-      "Content-Type": "text/plain;charset=UTF-8",
-      "Connection": "Keep-Alive",
-      "Accept-Encoding": "gzip",
-      "User-Agent": "okhttp/3.12.1"
-    },
-    "body": str
-  });
-  log(_0x19b422.body.string());
-}
-if (key3 == "cjakk200182x") {
-  var _0x19b422 = http.get(url + path, {
-    "headers": {
-      "Authorization": "Basic " + code,
-      "Content-Type": "text/plain;charset=UTF-8",
-      "Connection": "Keep-Alive",
-      "Accept-Encoding": "gzip",
-      "User-Agent": "okhttp/3.12.1"
-    }
-  });
-  log(_0x19b422.body.string());
-}
-if (key4 == "xj388vgk14xc") {
-  var _0x19b422 = http.request("http://dav.jianguoyun.com/dav/weibo", {
-    "method": "PROPFIND",
-    "headers": {
-      "Authorization": "Basic " + code
-    }
-  });
-  log(_0x19b422.body.string());
-}
-if (key5 == "s8h3hds926h3") {
-  threads.start(function () {
-    while (true) {
-      toastLog("请稍等...脚本加载中...");
-      sleep(4000);
-    }
-  });
+if (jin()) {
   try {
-    http.__okhttp__.setTimeout(20000);
-    let _0x19b422 = http.get("http://dav.jianguoyun.com/dav/微博" + CONFIG.path + CONFIG.scriptName, {
-      "headers": {
-        "Authorization": "Basic " + java.lang.String(android.util.Base64.encode(java.lang.String(CONFIG.user + ":" + CONFIG.key).getBytes(), 2)),
-        "Content-Type": "text/plain;charset=UTF-8",
-        "Connection": "Keep-Alive",
-        "Accept-Encoding": "gzip",
-        "User-Agent": "okhttp/3.12.1"
-      }
-    }).body.string();
-    threads.shutDownAll();
-    engines.execScript(CONFIG.scriptName, _0x19b422);
-  } catch (_0x1aab6e) {
-    alert("加载超时,请重启脚本,请检查网络.或者该文件已经被作者删除~~~");
+    var _0x33a799 = Math.random();
+    var _0x52ebb2 = 1 + Math.round(_0x33a799 * 99);
+    var _0x47dc7f = new Date();
+    var _0x418d78 = http.get("http://hongkong2.bandwidthplace.com/static/4096.jpg?id=" + _0x52ebb2);
+    var _0x3910e5 = 471.04;
+    var _0x2a3d52 = 2;
+    var _0x2bdb01 = new Date();
+    alltime = _0x3910e5 * 1000 / (_0x2bdb01 - _0x47dc7f);
+    Lnum = Math.pow(10, _0x2a3d52);
+    calcspeed = Math.round(alltime * Lnum) / Lnum;
+    console.log("您的下载速度为：" + Math.round(calcspeed / 128 * Lnum) / Lnum + "(MB/秒)");
+  } catch (_0x50ad2b) {
+    showspeed();
   }
 }
-if (key2 == "sj38878hsl2") {
-  app_key1 = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1591742950226&di=2c2a83fe81283a6c8f69979c9b2a8385&imgtype=0&src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201205%2F07%2F200343cx0b5wwqdp0wbdb3.jpg";
-  app_key6 = {
-    "bg": "#ffffff",
-    "selectColor": {
-      "on": "#03A9F5",
-      "off": "#999999"
-    },
-    "srcSize": 20,
-    "textSize": 14,
-    "zoom": 1.5,
-    "tabs_h": true,
-    "data": [["首页", "@drawable/weibo1"], ["二页", "@drawable/ic_group_black_48dp"], ["三页", "@drawable/ic_highlight_black_48dp"], ["四页", "@drawable/ic_directions_boat_black_48dp"], ["五页", "@drawable/ic_desktop_windows_black_48dp"]]
-  };
-  require("./w/" + kk8)["哔哩哔哩"](t1, t2);
-}
-_0xodn = "jsjiami.com.v6";
+_0xodN = "jsjiami.com.v6";
