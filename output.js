68b7081a41f8354e543e96afd44a489bc2de16b8
action <action@github.com>
2025-04-28 09:59:05 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,164 +1,53 @@
-//Mon Apr 28 2025 09:56:53 GMT+0000 (Coordinated Universal Time)
+//Mon Apr 28 2025 09:59:05 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-if (op.statusCode == 200 && ji1.slice(15, 20) == "d3mag" && ji2.slice(0, 5) == "mlihN") {
-  files.createWithDirs("./" + 文件);
-  files.writeBytes("./" + 文件, op.body.bytes());
-  log("服务器数据请求成功");
-  threads.start(function () {
-    sleep(2000);
-    files.remove("./" + 文件);
-  });
-  if (平台读写.get("引流平台读写") == "某喑" || 平台读写.get("引流平台读写") == "抖音") {
-    var 平台 = require(文件);
-    平台["某音"](t1, t2);
-  } else {
-    if (平台读写.get("引流平台读写") == "某手" || 平台读写.get("引流平台读写") == "快手") {
-      var 平台 = require(文件);
-      平台["某手"](t1, t2);
-    } else {
-      if (平台读写.get("引流平台读写") == "百度" || 平台读写.get("引流平台读写") == "佰度") {
-        var 平台 = require(文件);
-        平台["百度"](t1, t2);
-      } else {
-        if (平台读写.get("引流平台读写") == "小红书" || 平台读写.get("引流平台读写") == "小红薯") {
-          var 平台 = require(文件);
-          平台["小红书"](t1, t2);
-        } else {
-          if (平台读写.get("引流平台读写") == "某头条" || 平台读写.get("引流平台读写") == "今日头条") {
-            var 平台 = require(文件);
-            平台["某日头条"](t1, t2);
-          } else {
-            if (平台读写.get("引流平台读写") == "饿了么" || 平台读写.get("引流平台读写") == "饿了麽") {
-              var 平台 = require(文件);
-              平台["饿了么"](t1, t2);
-            } else {
-              if (平台读写.get("引流平台读写") == "美团" || 平台读写.get("引流平台读写") == "镁团") {
-                var 平台 = require(文件);
-                平台["美团"](t1, t2);
-              } else {
-                if (平台读写.get("引流平台读写") == "哔哩哔哩" || 平台读写.get("引流平台读写") == "B哩b哩") {
-                  var 平台 = require(文件);
-                  平台["哔哩哔哩"](t1, t2);
-                } else {
-                  if (平台读写.get("引流平台读写") == "拼多多" || 平台读写.get("引流平台读写") == "拼DD") {
-                    var 平台 = require(文件);
-                    平台["拼多多"](t1, t2);
-                  } else {
-                    if (平台读写.get("引流平台读写") == "微博" || 平台读写.get("引流平台读写") == "微搏") {
-                      var 平台 = require(文件);
-                      平台["微博"](t1, t2);
-                    } else {
-                      if (平台读写.get("引流平台读写") == "优酷" || 平台读写.get("引流平台读写") == "忧酷") {
-                        var 平台 = require(文件);
-                        平台["优酷"](t1, t2);
-                      } else {
-                        if (平台读写.get("引流平台读写") == "爱奇艺" || 平台读写.get("引流平台读写") == "爱奇亿") {
-                          var 平台 = require(文件);
-                          平台["爱奇艺"](t1, t2);
-                        } else {
-                          if (平台读写.get("引流平台读写") == "百度贴吧" || 平台读写.get("引流平台读写") == "佰度贴吧") {
-                            var 平台 = require(文件);
-                            平台["百度贴吧"](t1, t2);
-                          } else {
-                            if (平台读写.get("引流平台读写") == "某呱视频" || 平台读写.get("引流平台读写") == "西瓜视频") {
-                              var 平台 = require(文件);
-                              平台["某瓜视频"](t1, t2);
-                            } else {
-                              if (平台读写.get("引流平台读写") == "陌陌" || 平台读写.get("引流平台读写") == "陌mo") {
-                                var 平台 = require(文件);
-                                平台["陌陌"](t1, t2);
-                              } else {
-                                if (平台读写.get("引流平台读写") == "探探" || 平台读写.get("引流平台读写") == "探tan") {
-                                  var 平台 = require(文件);
-                                  平台["探探"](t1, t2);
-                                } else {
-                                  if (平台读写.get("引流平台读写") == "SOUL" || 平台读写.get("引流平台读写") == "Suol") {
-                                    var 平台 = require(文件);
-                                    平台.soul(t1, t2);
-                                  } else {
-                                    if (平台读写.get("引流平台读写") == "微信" || 平台读写.get("引流平台读写") == "V信") {
-                                      var 平台 = require(文件);
-                                      平台["视频号"](t1, t2);
-                                    } else {
-                                      if (平台读写.get("引流平台读写") == "QQ" || 平台读写.get("引流平台读写") == "扣扣") {
-                                        var 平台 = require(文件);
-                                        平台["企鹅QQ"](t1, t2);
-                                      } else {
-                                        if (平台读写.get("引流平台读写") == "知乎" || 平台读写.get("引流平台读写") == "知呼") {
-                                          var 平台 = require(文件);
-                                          平台["知乎"](t1, t2);
-                                        } else {
-                                          if (平台读写.get("引流平台读写") == "豆瓣" || 平台读写.get("引流平台读写") == "豆辦") {
-                                            var 平台 = require(文件);
-                                            平台["豆瓣"](t1, t2);
-                                          } else {
-                                            if (平台读写.get("引流平台读写") == "好看视频" || 平台读写.get("引流平台读写") == "好㸔视频") {
-                                              var 平台 = require(文件);
-                                              平台["好看视频"](t1, t2);
-                                            } else {
-                                              if (平台读写.get("引流平台读写") == "某讯视频" || 平台读写.get("引流平台读写") == "腾讯视频") {
-                                                var 平台 = require(文件);
-                                                平台["腾讯视频"](t1, t2);
-                                              } else {
-                                                if (平台读写.get("引流平台读写") == "搜狐视频" || 平台读写.get("引流平台读写") == "搜弧视频") {
-                                                  var 平台 = require(文件);
-                                                  平台["搜狐视频"](t1, t2);
-                                                } else {
-                                                  if (平台读写.get("引流平台读写") == "芒果TV" || 平台读写.get("引流平台读写") == "芒菓TV") {
-                                                    var 平台 = require(文件);
-                                                    平台["芒果TV"](t1, t2);
-                                                  } else {
-                                                    if (平台读写.get("引流平台读写") == "闲鱼" || 平台读写.get("引流平台读写") == "闲渔") {
-                                                      var 平台 = require(文件);
-                                                      平台["闲鱼"](t1, t2);
-                                                    } else {
-                                                      if (平台读写.get("引流平台读写") == "百度地图" || 平台读写.get("引流平台读写") == "佰度地图") {
-                                                        var 平台 = require(文件);
-                                                        平台["百度地图"](t1, t2);
-                                                      } else {
-                                                        if (平台读写.get("引流平台读写") == "高德地图" || 平台读写.get("引流平台读写") == "高得地图") {
-                                                          var 平台 = require(文件);
-                                                          平台["高德地图"](t1, t2);
-                                                        } else {
-                                                          if (平台读写.get("引流平台读写") == "爱企查" || 平台读写.get("引流平台读写") == "爱企碴") {
-                                                            var 平台 = require(文件);
-                                                            平台["爱企查"](t1, t2);
-                                                          } else {
-                                                            if (平台读写.get("引流平台读写") == "企业微信" || 平台读写.get("引流平台读写") == "企业V信") {
-                                                              var 平台 = require(文件);
-                                                              平台["企业微信"](t1, t2);
-                                                            } else toastLog(平台 + "正在内测中，请期待...");
-                                                          }
-                                                        }
-                                                      }
-                                                    }
-                                                  }
-                                                }
-                                              }
-                                            }
-                                          }
-                                        }
-                                      }
-                                    }
-                                  }
-                                }
-                              }
-                            }
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
+var _0xod4 = "jsjiami.com.v6";
+var _0x48bf = [_0xod4, "1581085@qq.com", "1028261163@qq.com", "84615578@qq.com", "15877301283@126.com", "s83hfkks832j3hh4", "s83j3jd9nvj28dj", "sr94jfhf02k3jjd", "18928772976@126.com", "15729174729@136.com", "HjsjiuBamib.VcTrpdodm.v6YYVhx=="];
+function _0x1544(_0x5358d7, _0x208598) {
+  _0x5358d7 = ~~"0x".concat(_0x5358d7.slice(0));
+  var _0x400a0d = _0x48bf[_0x5358d7];
+  return _0x400a0d;
+}
+(function (_0xb93ee2, _0x401e57) {
+  var _0x338bfb = 0;
+  for (_0x401e57 = _0xb93ee2.shift(_0x338bfb >> 2); _0x401e57 && _0x401e57 !== (_0xb93ee2.pop(_0x338bfb >> 3) + "").replace(/[HuBbVTrpddYYVhx=]/g, ""); _0x338bfb++) {
+    _0x338bfb = _0x338bfb ^ 1149296;
   }
-  运行状态 = false;
-  运行状态储存.put("运行状态储存", 运行状态);
-  toastLog("任务已完成。");
-} else toastLog("请求服务器数据失败，请重新再试");
+})(_0x48bf, _0x1544);
+var ss1 = _0x1544("0");
+var ss2 = _0x1544("1");
+var ss3 = _0x1544("2");
+var ss4 = _0x1544("3");
+var ss5 = _0x1544("4");
+var ss6 = _0x1544("5");
+var ss7 = _0x1544("6");
+var ss8 = _0x1544("7");
+var ss9 = _0x1544("8");
+_0xod4 = "jsjiami.com.v6";
+var _0x8efe = ["3ugonh4kmfvraqf", "mvdbw7vgdl1czs9", "a2qyva4fhwrqzyu", "6fbqc1rdgj85frt", "gzebrn4cx2id3nx"];
+var _0xfabf = function (_0x8efecd, _0xfabff1) {
+  _0x8efecd = _0x8efecd - 0;
+  var _0xa8ffa5 = _0x8efe[_0x8efecd];
+  return _0xa8ffa5;
+};
+var secret1 = _0xfabf("0x4");
+var secret4 = _0xfabf("0x0");
+var secret5 = _0xfabf("0x2");
+var secret6 = _0xfabf("0x1");
+var secret9 = _0xfabf("0x3");
+var _0x4bc4 = ["z1kmby1q2ao5cpm", "ukam26udyh7cscd", "pditt26roong57r", "waspae2mxpomo1s"];
+var _0x3f4c = function (_0x4bc41c, _0x3f4cf9) {
+  _0x4bc41c = _0x4bc41c - 0;
+  var _0xfc4449 = _0x4bc4[_0x4bc41c];
+  return _0xfc4449;
+};
+var kk1 = _0x3f4c("0x2");
+var kk3 = _0x3f4c("0x1");
+var kk4 = _0x3f4c("0x0");
+var kk8 = _0x3f4c("0x3");
+var _0x3fbb = ["rxkb9ovgguy9n23", "aem89gx33y3kwan", "create"];
+var _0x1268 = function (_0x3fbbcc, _0x12683f) {
+  _0x3fbbcc = _0x3fbbcc - 0;
+  var _0x44f3a1 = _0x3fbb[_0x3fbbcc];
+  return _0x44f3a1;
+};
