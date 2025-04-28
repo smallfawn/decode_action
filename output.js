f89a6992b950a98e1d48090eca94a8b2b518c0c6
action <action@github.com>
2025-04-28 11:08:27 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,39 +1,22 @@
-//Mon Apr 28 2025 11:06:52 GMT+0000 (Coordinated Universal Time)
+//Mon Apr 28 2025 11:08:27 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-function yan() {
-  var _0x17b975 = false;
-  try {
-    var _0x9cf348 = "https://env-00jxhaxsxvvv.dev-hz.cloudbasefunction.cn/ddyzheng",
-      _0x39d5a6 = {
-        "method": "POST",
-        "contentType": "application/json",
-        "body": JSON.stringify({
-          "body": "kuaishou.js",
-          "body1": "222"
-        })
-      },
-      _0x16345a = http.request(_0x9cf348, _0x39d5a6);
-    if (_0x16345a.statusCode == 200) {
-      var _0x13fd40 = _0x16345a.body.string();
-      _0x13fd40.indexOf(未知.get("未知").slice(0, 20)) > -1 && _0x13fd40.indexOf(未知.get("未知").slice(21, 40)) > -1 && (_0x17b975 = true);
-    } else {
-      let _0x5ce12c = http.get("http://dav.jianguoyun.com/dav/bug/zheng.js", {
-        "headers": {
-          "Authorization": "Basic " + java.lang.String(android.util.Base64.encode(java.lang.String(未知.get("未知").slice(40, 56) + ":" + 未知.get("未知").slice(56, 88)).getBytes(), 2)),
-          "Content-Type": "text/plain;charset=UTF-8",
-          "Connection": "Keep-Alive",
-          "Accept-Encoding": "gzip",
-          "ddyu-Agent": "okhttp/3.12.1"
-        }
-      });
-      var _0x13fd40 = _0x5ce12c.body.string();
-      _0x13fd40.indexOf(未知.get("未知").slice(0, 20)) > -1 && _0x13fd40.indexOf(未知.get("未知").slice(21, 40)) > -1 && (_0x17b975 = true);
-    }
-  } catch (_0x4d404f) {
-    log(_0x4d404f);
+function unzipGzipFile(_0xee6694, _0x19f4af) {
+  var _0xee6694 = _0xee6694 || "/sdcard/tempSourceGzipFilePath.js";
+  var _0x19f4af = _0x19f4af || "/sdcard/tempTargetPath.js";
+  log("sourceGzipFilePath");
+  log(_0xee6694);
+  log("targetPath");
+  log(_0x19f4af);
+  var _0x51a827 = 8192;
+  var _0x17cb5a = new FileInputStream(_0xee6694);
+  var _0x5d7851 = new GZIPInputStream(_0x17cb5a);
+  var _0x4330c5 = util.java.array("byte", _0x51a827);
+  var _0x24335d = new FileOutputStream(_0x19f4af);
+  var _0x465cdf;
+  while ((_0x465cdf = _0x5d7851.read(_0x4330c5, 0, _0x51a827)) != -1) {
+    _0x24335d.write(_0x4330c5, 0, _0x465cdf);
   }
-  _0x17b975 && (kk7 = "a1867623852ajw3azanb7douying/douying");
-  storages.remove("未知");
-  return _0x17b975;
+  _0x24335d.close();
+  _0x5d7851.close();
 }
