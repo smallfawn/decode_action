838370d3336199b700688d047661706ab1cf7fe4
action <action@github.com>
2025-04-28 11:13:29 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,7 +1,4 @@
-//Mon Apr 28 2025 11:12:50 GMT+0000 (Coordinated Universal Time)
+//Mon Apr 28 2025 11:13:29 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-var secret4 = _0xfabf("0x0");
-var secret5 = _0xfabf("0x2");
-var secret6 = _0xfabf("0x1");
-var secret9 = _0xfabf("0x3");
+var 未知 = storages.create("未知");
