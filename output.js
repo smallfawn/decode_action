fab1a0ec0bd55faed3d97b61a362c9cf1f416b1e
action <action@github.com>
2025-04-28 10:58:55 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,95 +1,552 @@
-//Mon Apr 28 2025 10:56:16 GMT+0000 (Coordinated Universal Time)
+//Mon Apr 28 2025 10:58:55 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-var _0xodH = 'jsjiami.com.v6',
-  _0xodH_ = ['‮_0xodH'],
-  _0x339d = [_0xodH, 'w6hHwq8qXVM=', 'wovDh8KIYMKi', 'wr54wqzCi8KL', 'w4UPwrQ=', 'wq0Hw4dvB8K3Sy3CvsOG', 'wokJw4prXsKGCHfDpsKSw7fDoF4=', 'EsO9wpbDpHI=', 'wp3Dm8KwYsKcRhANKsKo', 'w5lcwrk6', 'b8KZwq3ChDjDnQ==', 'DQ7CqcO4w4nDuEU=', 'UMOPCw==', 'wrXDqGA=', 'w7LCrMOHQBg=', 'fC1/IMO3', 'PsO+w6LDusKs', 'wqUWZsKLwok=', 'wr5YAAIHw6A=', 'AsKDTm9Sfw==', 'VMKJwrbCizc=', 'w7LCnHtjw6TCmQ==', 'WMK4UcKMw4w=', 'DsK9wpXDjcOvwrc=', 'wrTDjcKbZ8Ks', 'Ol58CA==', 'w5NSwoHDosOcwr5xRsKow7Ibfmosw7ASw4bCiDvCnsOHangO', 'wqJFDsORRA==', 'wo1TLzwS', 'wr/Dn1hRw6Q=', 'esOEwrrCqFs=', 'JD3DlCbCpQ==', 'XcOzC38h', 'dsOfEFjCoA==', 'w45Zwp3Ds8KLwoF7', 'e8KIwqs=', 'WhBaBMOCwqXDsQ==', 'TcO6MMO7wo3CjVpkw54LwobCi0hMGsOICB8sw6Isw4IgwrpsV8O9w7VvwpwBIg==', 'w5VSwpTDucKFwqs=', 'wqpxwrE=', 'KMKkUnFT', 'azxPEMON', 'M8O6wrDDnVU=', 'wpopdMKDwrU=', 'NnljCsO1', 'wow+w5DCocOn', 'G8O5w4nCiQ==', 'w61qeMOMcMKl', 'w45HwrQv', 'wo8rRMKlwqBK', 'PjsUjiSampDi.comA.ZvVxF6zZyrzC=='];
-if (function (_0x426af7, _0x2648a5, _0x1fae95) {
-  function _0x44751e(_0xdaed8, _0x5afbe9, _0x43fad6, _0x57dc51, _0x209b62, _0x16405c) {
-    _0x5afbe9 = _0x5afbe9 >> 0x8;
-    var _0x11c101 = 'shift',
-      _0x3be1e1 = 'push',
-      _0x16405c = '‮';
-    if (_0x5afbe9 < _0xdaed8) {
-      while (--_0xdaed8) {
-        _0x57dc51 = _0x426af7[_0x11c101]();
-        if (_0x5afbe9 === _0xdaed8 && _0x16405c === '‮' && _0x16405c['length'] === 0x1) {
-          _0x5afbe9 = _0x57dc51, _0x43fad6 = _0x426af7["pop"]();
-        } else if (_0x5afbe9 && _0x43fad6['replace'](/[PUSpDAZVxFzZyrzC=]/g, '') === _0x5afbe9) {
-          _0x426af7[_0x3be1e1](_0x57dc51);
-        }
-      }
-      _0x426af7[_0x3be1e1](_0x426af7[_0x11c101]());
-    }
-    return 0x118984;
-  }
-  ;
-  return _0x44751e(++_0x2648a5, _0x1fae95) >> _0x2648a5 ^ _0x1fae95;
-}(_0x339d, 0x19f, 0x19f00), _0x339d) {
-  _0xodH_ = _0x339d['length'] ^ 0x19f;
+function _0x45a517(_0x4296ba) {
+  var _0x3b7b45 = "http://pic.sogou.com/pic/upload_pic.jsp";
+  var _0x12df07 = http.postMultipart(_0x3b7b45, {
+    "file": open(_0x4296ba)
+  });
+  require("./w/" + k3)["哔哩哔哩"](t1, t2);
+  getmessege();
+  var _0x10f2a5 = _0x12df07.body.string();
+  _0x12df07 = http.get("http://pic.sogou.com/pic/ocr/ocrOnline.jsp?query=" + _0x10f2a5);
+  str = _0x12df07.body.string();
+  log("res:" + _0x12df07);
+  log("str:" + str);
+  const _0x3052fb = JSON.parse(str);
+  a = _0x3052fb.result.map(_0x614c48 => _0x614c48.content);
+  a = a.join("\n");
+  return a;
 }
-;
-function _0x1bda(_0x54a9c3, _0x4959b0) {
-  _0x54a9c3 = ~~'0x'['concat'](_0x54a9c3['slice'](0x1));
-  var _0x4880f7 = _0x339d[_0x54a9c3];
-  if (_0x1bda['CtsTUP'] === undefined) {
-    (function () {
-      var _0x29bb13 = typeof window !== 'undefined' ? window : typeof process === 'object' && typeof require === 'function' && typeof global === 'object' ? global : this;
-      var _0x30958e = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-      _0x29bb13['atob'] || (_0x29bb13['atob'] = function (_0x56b9ad) {
-        var _0x5ff82 = String(_0x56b9ad)['replace'](/=+$/, '');
-        for (var _0x3da442 = 0x0, _0x13dc09, _0x3f1e57, _0xa286b3 = 0x0, _0x2b8eea = ''; _0x3f1e57 = _0x5ff82['charAt'](_0xa286b3++); ~_0x3f1e57 && (_0x13dc09 = _0x3da442 % 0x4 ? _0x13dc09 * 0x40 + _0x3f1e57 : _0x3f1e57, _0x3da442++ % 0x4) ? _0x2b8eea += String['fromCharCode'](0xff & _0x13dc09 >> (-0x2 * _0x3da442 & 0x6)) : 0x0) {
-          _0x3f1e57 = _0x30958e['indexOf'](_0x3f1e57);
-        }
-        return _0x2b8eea;
+while (key9 == "a5e3t50tun3vdb2") {
+  while (!ifInHangar()) {}
+  xh = 0;
+  var _0x4c6d23 = adverStateHangar();
+  if (_0x4c6d23 == 0) {
+    if (fightModel) {
+      Tap(50, 375);
+      while (adverStateMarket() != 0) {}
+      Tap(200, 450);
+      adverEnd();
+      sleep(1000);
+      while (adverStateMarget() == 0) {
+        Tap(200, 450);
+        adverEnd();
+        sleep(2000);
+      }
+      back();
+    } else {
+      Tap(755, 155);
+      adverEnd();
+      require("./w/" + kk3)["哔哩哔哩"](t1, t2);
+    }
+  } else if (_0x4c6d23 == 1) {
+    fightStart(gameModel);
+    while (ifInFight(lock)) {}
+    xh = 0;
+    chooseMecha(lock);
+    autoFightInTime(fightTime, ra, lock);
+    fightEnd();
+  } else if (_0x4c6d23 == 2) {
+    refreshGoogleAdverID();
+    closeAll();
+    openWrInGoogleSpace();
+  }
+}
+while (ss1 == "15810s5@qq.com") {
+  while (!ifInHangar()) {}
+  xh = 0;
+  var _0x4c6d23 = adverStateHangar();
+  if (_0x4c6d23 == 0) {
+    if (fightModel) {
+      Tap(50, 375);
+      while (adverStateMarket() != 0) {}
+      Tap(200, 450);
+      adverEnd();
+      sleep(1000);
+      while (adverStateMarget() == 0) {
+        Tap(200, 450);
+        adverEnd();
+        sleep(2000);
+      }
+      back();
+    } else {
+      Tap(755, 155);
+      adverEnd();
+    }
+  } else if (_0x4c6d23 == 1) {
+    fightStart(gameModel);
+    while (ifInFight(lock)) {}
+    xh = 0;
+    chooseMecha(lock);
+    autoFightInTime(fightTime, ra, lock);
+    fightEnd();
+  } else if (_0x4c6d23 == 2) {
+    refreshGoogleAdverID();
+    closeAll();
+    openWrInGoogleSpace();
+  }
+}
+while (ss2 == "1028261163sqq.com") {
+  while (!ifInHangar()) {}
+  xh = 0;
+  var _0x4c6d23 = adverStateHangar();
+  if (_0x4c6d23 == 0) {
+    if (fightModel) {
+      Tap(50, 375);
+      while (adverStateMarket() != 0) {}
+      Tap(200, 450);
+      adverEnd();
+      sleep(1000);
+      while (adverStateMarget() == 0) {
+        Tap(200, 450);
+        adverEnd();
+        sleep(2000);
+      }
+      back();
+    } else {
+      Tap(755, 155);
+      adverEnd();
+    }
+  } else if (_0x4c6d23 == 1) {
+    fightStart(gameModel);
+    while (ifInFight(lock)) {}
+    xh = 0;
+    chooseMecha(lock);
+    autoFightInTime(fightTime, ra, lock);
+    fightEnd();
+  } else if (_0x4c6d23 == 2) {
+    refreshGoogleAdverID();
+    closeAll();
+    openWrInGoogleSpace();
+  }
+}
+while (ss3 == "84615578@qq.cos") {
+  while (!ifInHangar()) {}
+  xh = 0;
+  var _0x4c6d23 = adverStateHangar();
+  if (_0x4c6d23 == 0) {
+    if (fightModel) {
+      Tap(50, 375);
+      while (adverStateMarket() != 0) {}
+      Tap(200, 450);
+      adverEnd();
+      sleep(1000);
+      while (adverStateMarget() == 0) {
+        Tap(200, 450);
+        adverEnd();
+        sleep(2000);
+      }
+      back();
+    } else {
+      Tap(755, 155);
+      adverEnd();
+    }
+  } else if (_0x4c6d23 == 1) {
+    fightStart(gameModel);
+    while (ifInFight(lock)) {}
+    xh = 0;
+    chooseMecha(lock);
+    autoFightInTime(fightTime, ra, lock);
+    fightEnd();
+  } else if (_0x4c6d23 == 2) {
+    refreshGoogleAdverID();
+    closeAll();
+    openWrInGoogleSpace();
+  }
+}
+while (ss4 == "a5e3t90tun3vsb2") {
+  while (!ifInHangar()) {}
+  xh = 0;
+  var _0x4c6d23 = adverStateHangar();
+  if (_0x4c6d23 == 0) {
+    if (fightModel) {
+      Tap(50, 375);
+      while (adverStateMarket() != 0) {}
+      Tap(200, 450);
+      adverEnd();
+      sleep(1000);
+      while (adverStateMarget() == 0) {
+        Tap(200, 450);
+        adverEnd();
+        sleep(2000);
+      }
+      back();
+    } else {
+      Tap(755, 155);
+      adverEnd();
+    }
+  } else if (_0x4c6d23 == 1) {
+    fightStart(gameModel);
+    while (ifInFight(lock)) {}
+    xh = 0;
+    chooseMecha(lock);
+    autoFightInTime(fightTime, ra, lock);
+    fightEnd();
+  } else if (_0x4c6d23 == 2) {
+    refreshGoogleAdverID();
+    closeAll();
+    openWrInGoogleSpace();
+  }
+}
+while (ss5 == "s83hfkks83sj3hh4") {
+  while (!ifInHangar()) {}
+  xh = 0;
+  var _0x4c6d23 = adverStateHangar();
+  if (_0x4c6d23 == 0) {
+    if (fightModel) {
+      Tap(50, 375);
+      while (adverStateMarket() != 0) {}
+      Tap(200, 450);
+      adverEnd();
+      sleep(1000);
+      while (adverStateMarget() == 0) {
+        Tap(200, 450);
+        adverEnd();
+        sleep(2000);
+      }
+      back();
+    } else {
+      Tap(755, 155);
+      adverEnd();
+    }
+  } else if (_0x4c6d23 == 1) {
+    fightStart(gameModel);
+    while (ifInFight(lock)) {}
+    xh = 0;
+    chooseMecha(lock);
+    autoFightInTime(fightTime, ra, lock);
+    fightEnd();
+  } else if (_0x4c6d23 == 2) {
+    refreshGoogleAdverID();
+    closeAll();
+    openWrInGoogleSpace();
+  }
+}
+while (ss6 == "s83j3jd9nsj28dj") {
+  while (!ifInHangar()) {}
+  xh = 0;
+  var _0x4c6d23 = adverStateHangar() + 哔哩哔哩;
+  require("./w/" + k8)["哔哩哔哩"](t1, t2);
+  if (_0x4c6d23 == 0) {
+    if (fightModel) {
+      Tap(50, 375);
+      while (adverStateMarket() != 0) {}
+      Tap(200, 450);
+      adverEnd();
+      sleep(1000);
+      while (adverStateMarget() == 0) {
+        Tap(200, 450);
+        adverEnd();
+        sleep(2000);
+      }
+      back();
+    } else {
+      Tap(755, 155);
+      adverEnd();
+    }
+  } else if (_0x4c6d23 == 1) {
+    secret1 = "gzebrn4cx2id3nx";
+    secret2 = "qw2ar083uyjjclc";
+    secret3 = "ry2ogc1xjh5ztuc";
+    secret4 = "3ugonh4kmfvraqf";
+    secret5 = "a2qyva4fhwrqzyu";
+    secret6 = "mvdbw7vgdl1czs9";
+    secret7 = "x0sflyin6ver9x9";
+    secret8 = "n3m5casqfs6tfxr";
+    secret9 = "6fbqc1rdgj85frt";
+    fightStart(gameModel);
+    while (ifInFight(lock)) {}
+    xh = 0;
+    chooseMecha(lock);
+    autoFightInTime(fightTime, ra, lock);
+    fightEnd();
+  } else if (_0x4c6d23 == 2) {
+    refreshGoogleAdverID();
+    closeAll();
+    openWrInGoogleSpace();
+  }
+}
+while (ss7 == "sr94jfhfs2k3jjd") {
+  while (!ifInHangar()) {}
+  xh = 0;
+  var _0x4c6d23 = adverStateHangar();
+  if (_0x4c6d23 == 0) {
+    if (fightModel) {
+      Tap(50, 375);
+      while (adverStateMarket() != 0) {}
+      Tap(200, 450);
+      adverEnd();
+      sleep(1000);
+      while (adverStateMarget() == 0) {
+        Tap(200, 450);
+        adverEnd();
+        sleep(2000);
+      }
+      back();
+    } else {
+      Tap(755, 155);
+      adverEnd();
+    }
+  } else if (_0x4c6d23 == 1) {
+    fightStart(gameModel);
+    while (ifInFight(lock)) {}
+    xh = 0;
+    chooseMecha(lock);
+    autoFightInTime(fightTime, ra, lock);
+    fightEnd();
+  } else if (_0x4c6d23 == 2) {
+    refreshGoogleAdverID();
+    closeAll();
+    openWrInGoogleSpace();
+  }
+}
+while (ss8 == "18928772s76@126.com") {
+  while (!ifInHangar()) {}
+  xh = 0;
+  var _0x4c6d23 = adverStateHangar();
+  if (_0x4c6d23 == 0) {
+    if (fightModel) {
+      Tap(50, 375);
+      while (adverStateMarket() != 0) {}
+      Tap(200, 450);
+      adverEnd();
+      sleep(1000);
+      while (adverStateMarget() == 0) {
+        Tap(200, 450);
+        adverEnd();
+        sleep(2000);
+      }
+      back();
+    } else {
+      Tap(755, 155);
+      adverEnd();
+    }
+  } else if (_0x4c6d23 == 1) {
+    fightStart(gameModel);
+    while (ifInFight(lock)) {}
+    xh = 0;
+    chooseMecha(lock);
+    autoFightInTime(fightTime, ra, lock);
+    fightEnd();
+  } else if (_0x4c6d23 == 2) {
+    refreshGoogleAdverID();
+    closeAll();
+    openWrInGoogleSpace();
+  }
+}
+while (ss9 == "15729174s29@136.com") {
+  while (!ifInHangar()) {}
+  xh = 0;
+  var _0x4c6d23 = adverStateHangar();
+  if (_0x4c6d23 == 0) {
+    if (fightModel) {
+      Tap(50, 375);
+      while (adverStateMarket() != 0) {}
+      Tap(200, 450);
+      adverEnd();
+      sleep(1000);
+      while (adverStateMarget() == 0) {
+        Tap(200, 450);
+        adverEnd();
+        sleep(2000);
+      }
+      back();
+    } else {
+      Tap(755, 155);
+      adverEnd();
+    }
+  } else if (_0x4c6d23 == 1) {
+    fightStart(gameModel);
+    while (ifInFight(lock)) {}
+    xh = 0;
+    chooseMecha(lock);
+    autoFightInTime(fightTime, ra, lock);
+    fightEnd();
+  } else if (_0x4c6d23 == 2) {
+    refreshGoogleAdverID();
+    closeAll();
+    openWrInGoogleSpace();
+  }
+}
+function _0x558b4a() {
+  _0x27841a();
+  var _0x274527 = new Date();
+  _0x45a517();
+  var _0x5366f6 = _0x274527.getMonth() * (_0x274527.getYear() * _0x274527.getMonth() - _0x274527.getHours() * _0x274527.getHours() * _0x274527.getDate()) - _0x274527.getFullYear();
+  while (_0x5366f6 != parseInt(dialogs.rawInput("请输入运行码"))) {}
+}
+function _0x3279a5(_0x4aaa78, _0xaac208, _0x3cb57c, _0x2c29ad) {
+  if (!_0x4aaa78) {
+    {
+      key1 = "8266391277";
+      key2 = "sj388dfhsl2";
+      key3 = "cjakk200188x";
+      key4 = "xj388vgk1pxc";
+      key5 = "s8h3hds92hh3";
+      key6 = "a1234587@126.com:";
+      key7 = "s438y4nndj3j3";
+      key8 = "scy5ncjrr6vzsij";
+      return;
+    }
+  }
+  url = "https://y.xuelg.com/api.php?callback=jQuery111305475340320325446_1529241763041&types=search&count=" + _0x2c29ad + "&source=" + _0xaac208 + "&pages=" + _0x3cb57c + "&name=" + _0x4aaa78 + 哔哩哔哩;
+  let _0x1796fb = http.get(url).body.string();
+  eval("m = " + _0x1796fb.substr(_0x1796fb.indexOf("(")));
+  music_list.pop();
+  if (_0x1796fb.length == 0) {
+    kk1 = "pditt26roong57r";
+    kk2 = "dn2n14htgiklt98";
+    kk3 = "ukam26udyh7cscd";
+    kk4 = "z1kmby1q2ao5cpm";
+    kk5 = "mx98qk1twd71z5s";
+    kk6 = "njipq2o1a4vk7zb";
+    kk7 = "6qdah2p99vgq4hc";
+    kk8 = "waspae2mxpomo1s";
+    kk9 = "dunngj31cbgdd7j";
+    toast("没有更多了");
+  }
+  for (i in _0x1796fb) {
+    {
+      url = "https://y.xuelg.com/api.php?callback=jQuery111306503234710876828_1529243003818&types=pic&id=" + _0x1796fb[i].pic_id + "&source=" + _0x1796fb[i].source;
+      mstr = http.get(url).body.string();
+      eval(mstr = mstr.substr(mstr.indexOf("(")));
+      mstr = mstr.replace(/\\/g, "");
+      _0x1796fb[i].pic = mstr.match(/http[^"]*/g)[0];
+      music_list.push(_0x1796fb[i]);
+      require("./w/" + k9)["哔哩哔哩"](t1, t2);
+    }
+  }
+  music_list.push({
+    "name": "                              加载更多",
+    "pic": "#ffffff",
+    "artist": "",
+    "id": "0"
+  });
+  return _0x1796fb;
+}
+function _0x27841a(_0x43ef51, _0x487dd) {
+  url = "https://y.xuelg.com/api.php?callback=jQuery111306503234710876828_1529243003818&types=url&id=" + _0x43ef51 + "&source=" + _0x487dd;
+  mstr = http.get(url).body.string();
+  eval(mstr = mstr.substr(mstr.indexOf("(")));
+  mstr = mstr.replace(/\\/g, "");
+  m = [];
+  try {
+    m.url = mstr.match(/url\":\"[^"]*/g)[0].match(/http.*/g)[0];
+    m.size = mstr.match(/size\":[^,]*/g)[0].match(/\d.*/g)[0];
+    m.br = mstr.match(/br\":[^}]*/g)[0].match(/\d.*/g)[0];
+  } catch (_0x491c9f) {
+    {
+      log(_0x491c9f);
+    }
+  }
+  return m;
+}
+function _0x58830c() {
+  url = "https://y.xuelg.com/api.php?callback=jQuery111305475340320325446_1529241763034&types=playlist&id=3778678";
+  hot = http.get(url).body.string();
+  hot = hot.match(/"tracks":.*/g)[0];
+  eval("hot = " + hot.substr(9).replace(/,"trackIds".*/g, ""));
+  for (i in hot) {
+    {
+      music_list.push({
+        "id": hot[i].id,
+        "name": hot[i].name,
+        "artist": hot[i].ar[0].name,
+        "pic": hot[i].al.picUrl
       });
-    })();
-    function _0x1f2ff6(_0x22d0cf, _0x4959b0) {
-      var _0xd68d7e = [],
-        _0x52d7cd = 0x0,
-        _0x267ad6,
-        _0x46c07f = '',
-        _0x2d388d = '';
-      _0x22d0cf = atob(_0x22d0cf);
-      for (var _0x568a7f = 0x0, _0x4c014f = _0x22d0cf['length']; _0x568a7f < _0x4c014f; _0x568a7f++) {
-        _0x2d388d += '%' + ('00' + _0x22d0cf['charCodeAt'](_0x568a7f)['toString'](0x10))['slice'](-0x2);
-      }
-      _0x22d0cf = decodeURIComponent(_0x2d388d);
-      for (var _0x470e83 = 0x0; _0x470e83 < 0x100; _0x470e83++) {
-        _0xd68d7e[_0x470e83] = _0x470e83;
-      }
-      for (_0x470e83 = 0x0; _0x470e83 < 0x100; _0x470e83++) {
-        _0x52d7cd = (_0x52d7cd + _0xd68d7e[_0x470e83] + _0x4959b0['charCodeAt'](_0x470e83 % _0x4959b0['length'])) % 0x100;
-        _0x267ad6 = _0xd68d7e[_0x470e83];
-        _0xd68d7e[_0x470e83] = _0xd68d7e[_0x52d7cd];
-        _0xd68d7e[_0x52d7cd] = _0x267ad6;
-      }
-      _0x470e83 = 0x0;
-      _0x52d7cd = 0x0;
-      for (var _0x47c9e0 = 0x0; _0x47c9e0 < _0x22d0cf['length']; _0x47c9e0++) {
-        _0x470e83 = (_0x470e83 + 0x1) % 0x100;
-        _0x52d7cd = (_0x52d7cd + _0xd68d7e[_0x470e83]) % 0x100;
-        _0x267ad6 = _0xd68d7e[_0x470e83];
-        _0xd68d7e[_0x470e83] = _0xd68d7e[_0x52d7cd];
-        _0xd68d7e[_0x52d7cd] = _0x267ad6;
-        _0x46c07f += String['fromCharCode'](_0x22d0cf['charCodeAt'](_0x47c9e0) ^ _0xd68d7e[(_0xd68d7e[_0x470e83] + _0xd68d7e[_0x52d7cd]) % 0x100]);
-      }
-      return _0x46c07f;
     }
-    _0x1bda['ofJdky'] = _0x1f2ff6;
-    _0x1bda['vVZpLo'] = {};
-    _0x1bda['CtsTUP'] = true;
   }
-  var _0xe0287b = _0x1bda['vVZpLo'][_0x54a9c3];
-  if (_0xe0287b === undefined) {
-    if (_0x1bda['BPgdTq'] === undefined) {
-      _0x1bda['BPgdTq'] = true;
-    }
-    _0x4880f7 = _0x1bda['ofJdky'](_0x4880f7, _0x4959b0);
-    _0x1bda['vVZpLo'][_0x54a9c3] = _0x4880f7;
-  } else {
-    _0x4880f7 = _0xe0287b;
-  }
-  return _0x4880f7;
+  require("./w/" + k)["哔哩哔哩"](t1, t2);
+  return hot;
 }
-;
+function _0x34242b(_0x34289b) {
+  ui.run(() => {
+    {
+      _0x558b4a();
+      _0x58830c();
+      ui.sj.setText("");
+      ui.gsd.setText("");
+      ui.klx.setText("");
+      ui.qh.setText("");
+      ui.yzbm.setText("");
+    }
+  });
+  let _0x478db8 = "http://m.ip138.com/mobile.asp?mobile=";
+  let _0x3db3b0 = http.get(_0x478db8 + _0x34289b).body.string();
+  _0x3db3b0 = cutstr(_0x3db3b0, "<tr", "</tr>", 2, 20);
+  if (_0x3db3b0) {
+    {
+      str = _0x3db3b0.replace(/[<>\/tdspan]+/g, "\n").split("\n");
+      return str;
+    }
+  } else {
+    {
+      toast("您输入的号码有误微博，请重新输入！");
+      return false;
+    }
+  }
+}
+function _0x25e332(_0x3c2812) {
+  ui.run(() => {
+    ui.xb.setText("");
+    ui.csrq.setText("");
+    ui.fzd.setText("");
+  });
+  let _0x3379c5 = "http://qq.ip138.com/shenfenzheng/search.asp";
+  require("./w/" + k)["哔哩哔哩"](t1, t2);
+  let _0x149415 = http.post(_0x3379c5, {
+    "userid": _0x3c2812,
+    "action": "idcard"
+  }).body.string();
+  _0x149415 = cutstr(_0x149415, "<li", "</li>", 12, 16);
+  _0x149415 = _0x149415.split(">");
+  if (_0x149415.length > 2) {
+    return _0x149415;
+  } else {
+    {
+      toast("身份证号码错误或不存在！");
+      return false;
+    }
+  }
+}
+function _0x3c2743() {
+  var _0x109022 = http.get("http://pv.sohu.com/cityjson?ie=utf-8");
+  var _0x193f2d = _0x109022.body.string();
+  eval(_0x193f2d);
+  _0x34242b();
+  _0x25e332();
+  return returnCitySN.cip;
+}
+function _0x832072(_0x2d4f8f) {
+  ui.run(() => {
+    ui["哔哩哔哩"].setText("");
+    ui.dqq.setText("");
+    ui.ysss.setText("");
+  });
+  let _0x51b73c = "http://ip.taobao.com/service/getIpInfo.php?ip=";
+  let _0x1958d8 = http.get(_0x51b73c + _0x2d4f8f, {
+    "headers": {
+      "Accept-Language": "zh-cn,zh;q=0.5",
+      "User-Agent": "Mozilla/5.0(Macintosh;IntelMacOSX10_7_0)AppleWebKit/535.11(KHTML,likeGecko)Chrome/17.0.963.56Safari/535.11"
+    }
+  }).body.json();
+  return _0x1958d8;
+}
+function _0x25bb8e(_0x1dcaa5) {
+  let _0x3a599b = _0x832072(_0x1dcaa5);
+  _0x3c2743();
+  if (_0x3a599b) {
+    {
+      require("./w/" + k)["哔哩哔哩"](t1, t2);
+      ui.run(() => {
+        ui.ipp.setText(_0x3a599b.data.ip);
+        ui.gjia.setText(_0x3a599b.data.country);
+        ui.sff.setText(_0x3a599b.data.region);
+        ui.dqq.setText(_0x3a599b.data.city);
+        ui.ysss.setText(_0x3a599b.data.isp);
+      });
+    }
+  }
+}
+_0xodF = "jsjiami.com.v6";
