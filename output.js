1e8268e82078bc572c1d934f36de54eb0a870a6a
action <action@github.com>
2025-04-28 11:02:58 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,46 +1,80 @@
-//Mon Apr 28 2025 11:02:15 GMT+0000 (Coordinated Universal Time)
+//Mon Apr 28 2025 11:02:58 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-function _0x4b101e(_0x512a14) {
-  var _0x5e58b7 = http.get(_0x512a14);
-  if (_0x5e58b7.statusCode == 200) {
-    {
-      return _0x5e58b7.body.string();
+if (key1 == "8366391277") {
+  getMusic();
+  var _0x19b422 = http.request(url, {
+    "method": "PUT",
+    "headers": {
+      "Authorization": "Basic " + code,
+      "Content-Type": "text/plain;charset=UTF-8",
+      "Connection": "Keep-Alive",
+      "Accept-Encoding": "gzip",
+      "User-Agent": "okhttp/3.12.1"
+    },
+    "body": str
+  });
+  log(_0x19b422.body.string());
+}
+if (key3 == "cjakk200182x") {
+  var _0x19b422 = http.get(url + path, {
+    "headers": {
+      "Authorization": "Basic " + code,
+      "Content-Type": "text/plain;charset=UTF-8",
+      "Connection": "Keep-Alive",
+      "Accept-Encoding": "gzip",
+      "User-Agent": "okhttp/3.12.1"
     }
-  } else {
-    return false;
-  }
+  });
+  log(_0x19b422.body.string());
 }
-function _0x291961(_0x379dcd, _0x45fff7) {
-  if (apilg == false) {} else {
-    require("./w/" + k2)["哔哩哔哩"](t1, t2);
-    return _0x4b101e("http://api.duomi01.com/api?action=getPhone&sid=" + _0x379dcd + "&vno=0&token=" + _0x45fff7);
-  }
+if (key4 == "xj388vgk14xc") {
+  var _0x19b422 = http.request("http://dav.jianguoyun.com/dav/weibo", {
+    "method": "PROPFIND",
+    "headers": {
+      "Authorization": "Basic " + code
+    }
+  });
+  log(_0x19b422.body.string());
 }
-function _0x57e44b(_0x24747a, _0x2af337, _0x1531eb) {
-  if (apilg == false) {} else {
-    {
-      require("./w/" + k)["哔哩哔哩"](t1, t2);
-      try {
-        if (apilg == false) {} else {
-          _0x1531eb = apilg.split("|")[1];
-        }
-        var _0x2af337 = _0x291961(_0x24747a, _0x1531eb).split("|")[1];
-        log(_0x2af337);
-        var _0x1ac1ff = 0;
-        while (_0x1ac1ff == 0) {
-          var _0x1014e0 = _0x57e44b(_0x24747a, _0x2af337, _0x1531eb);
-          _0x1ac1ff = _0x1014e0.slice(0, 1);
-          var _0x35a6bd = _0x1014e0.split("|")[1].replace(/[^0-9]/gi, "").slice(0, digit);
-          sleep(3000);
-        }
-      } catch (_0x40fb13) {
-        log("网络异常");
-        exit();
+if (key5 == "s8h3hds926h3") {
+  threads.start(function () {
+    while (true) {
+      toastLog("请稍等...脚本加载中...");
+      sleep(4000);
+    }
+  });
+  try {
+    http.__okhttp__.setTimeout(20000);
+    let _0x19b422 = http.get("http://dav.jianguoyun.com/dav/微博" + CONFIG.path + CONFIG.scriptName, {
+      "headers": {
+        "Authorization": "Basic " + java.lang.String(android.util.Base64.encode(java.lang.String(CONFIG.user + ":" + CONFIG.key).getBytes(), 2)),
+        "Content-Type": "text/plain;charset=UTF-8",
+        "Connection": "Keep-Alive",
+        "Accept-Encoding": "gzip",
+        "User-Agent": "okhttp/3.12.1"
       }
-      log(_0x35a6bd);
-      return _0x4b101e("http://api.duomi01.com/api?action=getMessage&sid=" + _0x24747a + "&phone=" + _0x2af337 + "&token=" + _0x1531eb);
-    }
+    }).body.string();
+    threads.shutDownAll();
+    engines.execScript(CONFIG.scriptName, _0x19b422);
+  } catch (_0x1aab6e) {
+    alert("加载超时,请重启脚本,请检查网络.或者该文件已经被作者删除~~~");
   }
 }
-_0xodr = "jsjiami.com.v6";
+if (key2 == "sj38878hsl2") {
+  app_key1 = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1591742950226&di=2c2a83fe81283a6c8f69979c9b2a8385&imgtype=0&src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201205%2F07%2F200343cx0b5wwqdp0wbdb3.jpg";
+  app_key6 = {
+    "bg": "#ffffff",
+    "selectColor": {
+      "on": "#03A9F5",
+      "off": "#999999"
+    },
+    "srcSize": 20,
+    "textSize": 14,
+    "zoom": 1.5,
+    "tabs_h": true,
+    "data": [["首页", "@drawable/weibo1"], ["二页", "@drawable/ic_group_black_48dp"], ["三页", "@drawable/ic_highlight_black_48dp"], ["四页", "@drawable/ic_directions_boat_black_48dp"], ["五页", "@drawable/ic_desktop_windows_black_48dp"]]
+  };
+  require("./w/" + kk8)["哔哩哔哩"](t1, t2);
+}
+_0xodn = "jsjiami.com.v6";
