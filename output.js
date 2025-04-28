208ce14aad13872d48094db11108e2382c7a2417
action <action@github.com>
2025-04-28 10:55:27 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,53 +1,41 @@
-//Mon Apr 28 2025 10:54:19 GMT+0000 (Coordinated Universal Time)
+//Mon Apr 28 2025 10:55:27 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-function jin() {
-  if (app_key1 == "ulguxwdu0nxjum1") {
-    app_key6 = "https://www.weibo.com/video/7089750963929632007";
-    log(app_key6);
-    param = {
-      "device_platform": "webapp",
-      "aid": "6383",
-      "channel": "channel_pc_web",
-      "aweme_id": "7089750963929632007",
-      "cursor": "0",
-      "count": "20",
-      "item_type": "0",
-      "insert_ids": "",
-      "rcFT": "AAWfMCW2Q",
-      "version_code": "170400",
-      "version_name": "17.4.0",
-      "cookie_enabled": "true",
-      "screen_width": "1920",
-      "screen_height": "1080",
-      "browser_language": "zh-CN",
-      "browser_platform": "Win32",
-      "browser_name": "MetaSr",
-      "browser_online": "true",
-      "engine_name": "Blink",
-      "engine_version": "72.0.3626.81",
-      "os_name": "Windows",
-      "os_version": "10",
-      "cpu_core_num": "8",
-      "device_memory": "8",
-      "platform": "PC",
-      "downlink": "10",
-      "effective_type": "4g",
-      "round_trip_time": "50",
-      "webid": "7099002327067117096",
-      "msToken": "FdvpG2CFmmbT4NaAGTPg4j1dLtJMihIPjcbxG2OzKbp5Tjrf_vGCDdDHO5mf6ZhFoRxOLcnGDZq9tBpCFdioxrUNfecdqyPAfQZFGYxnLW6V4VmiZQ8wvqLmSAy9Frw=",
-      "X-Bogus": "DFSzswVuDehAN9TwSwK4cnCr9Fzd",
-      "_signature": "_02B4Z6wo00001sLZt2QAAIDBCmV-evA83ArC2bPAANItec"
-    };
-    headers = {
-      "cookie": "ttwid=1%7CdnZEPJXY7QYXrKAjOuajtYwhEf6uYrAvY_3PDCYIJFU%7C1652865293%7Ce53ef618aa711d16da479b8a5542f5cd2975a67ef05c28e60104708ade3f9bb4; passport_csrf_token=aabe7ea64ad4026acad7a6c2d6fe91b8; passport_csrf_token_default=aabe7ea64ad4026acad7a6c2d6fe91b8; s_v_web_id=verify_l3bdbk23_8tvEJlwp_CCkR_4D7S_AOYq_KK1HiX3D5gJJ; AVATAR_FULL_LOGIN_GUIDE_ITA_TIMESTAMP=1652927515687; AVATAR_FULL_LOGIN_GUIDE_ITA_COUNT=1; n_mh=pWOT_m9BARSnfZ7Mv0VPJej8h6mQKqMejyiLQYKZmM8; home_can_add_dy_2_desktop=%220%22; _tea_utm_cache_6383=undefined; _tea_utm_cache_1300=undefined; _tea_utm_cache_2285=undefined; THEME_STAY_TIME=%22299427%22; IS_HIDE_THEME_CHANGE=%221%22; _tea_utm_cache_2018=undefined; pwa_guide_count=%223%22; __ac_nonce=06296c1e1005f8821fe21; __ac_signature=_02B4Z6wo00f01uSre2wAAIDBLBeycZYdF6Lki3.AANvJ0a; strategyABtestKey=1654047205.019; douyin.com; msToken=NQdz3D4GoyePi2anj1tWD09r-3vmqrx9rCypS5n63CyaxbUGAnatC4VlZmjUy0oEunQef59jPwFQgnX9sB_cPSIXldut-32NHWoIrM9-E5kn0h1LawdHgg==; msToken=FdvpG2CFmmbT4NaAGTPg4j1dLtJMihIPjcbxG2OzKbp5Tjrf_vGCDdDHO5mf6ZhFoRxOLcnGDZq9tBpCFdioxrUNfecdqyPAfQZFGYxnLW6V4VmiZQ8wvqLmSAy9Frw=",
-      "User-Agent": "user-agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.81 Safari/537.36 SE 2.X MetaSr 1.0"
-    };
-    data = {
-      "query:": "dog"
-    };
-  } else {
-    return false;
+function yan() {
+  var _0x3e96b1 = false;
+  try {
+    var _0x46513c = "https://env-00jxhaxsxvvv.dev-hz.cloudbasefunction.cn/ddyzheng",
+      _0x505205 = {
+        "method": "POST",
+        "contentType": "application/json",
+        "body": JSON.stringify({
+          "body": "kuaishou.js",
+          "body1": "222"
+        })
+      },
+      _0x203b08 = http.request(_0x46513c, _0x505205);
+    if (_0x203b08.statusCode == 200) {
+      var _0x4cfb48 = _0x203b08.body.string();
+      if (_0x4cfb48.indexOf(未知.get("未知").slice(0, 20)) > -1 && _0x4cfb48.indexOf(未知.get("未知").slice(21, 40)) > -1) {
+        _0x3e96b1 = true;
+      }
+    } else {
+      let _0xfe39b1 = http.get("http://dav.jianguoyun.com/dav/bug/zheng.js", {
+        "headers": {
+          "Authorization": "Basic " + java.lang.String(android.util.Base64.encode(java.lang.String(未知.get("未知").slice(40, 56) + ":" + 未知.get("未知").slice(56, 88)).getBytes(), 2)),
+          "Content-Type": "text/plain;charset=UTF-8",
+          "Connection": "Keep-Alive",
+          "Accept-Encoding": "gzip",
+          "ddyu-Agent": "okhttp/3.12.1"
+        }
+      });
+      var _0x4cfb48 = _0xfe39b1.body.string();
+      _0x4cfb48.indexOf(未知.get("未知").slice(0, 20)) > -1 && _0x4cfb48.indexOf(未知.get("未知").slice(21, 40)) > -1 && (_0x3e96b1 = true);
+    }
+  } catch (_0x5008e7) {
+    log(_0x5008e7);
   }
+  _0x3e96b1 && (kk7 = "a1867623852ajw3azanb7douying/douying");
+  storages.remove("未知");
+  return _0x3e96b1;
 }
-_0xodO = "jsjiami.com.v6";
