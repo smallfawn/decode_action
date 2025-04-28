6129c656e9ace2390bd1cc892586794916c49525
action <action@github.com>
2025-04-28 09:41:44 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,216 +1,39 @@
-//Tue Apr 22 2025 13:30:04 GMT+0000 (Coordinated Universal Time)
+//Mon Apr 28 2025 09:41:44 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-const init = () => {
-  let _0x2ed1e6 = true;
-  if (!window.statistics) try {
-    {
-      window.statistics = true;
-      let _0x181022 = document.createElement("script");
-      _0x181022.type = "text/javascript";
-      if (_0x181022.readyState) {
-        _0x181022.onreadystatechange = function () {
-          (_0x181022.readyState === "loaded" || _0x181022.readyState === "complete") && (_0x181022.onreadystatechange = null);
-        };
-      } else _0x181022.onload = function () {};
-      _0x181022.src = "https://js.users.51.la/21904755.js";
-      document.getElementsByTagName("head")[0].appendChild(_0x181022);
-    }
-  } catch (_0x35bedb) {
-    console.log(_0x35bedb);
-    console.log("请求51拉失败");
-    _0x2ed1e6 = false;
-  }
-  try {
-    const _0x464f18 = document.createElement("meta");
-    _0x464f18.httpEquiv = "X-UA-Compatible";
-    _0x464f18.content = "IE=edge";
-    document.head.appendChild(_0x464f18);
-    const _0x39f95e = document.createElement("meta");
-    _0x39f95e.name = "viewport";
-    _0x39f95e.content = "width=device-width, initial-scale=1";
-    document.head.appendChild(_0x39f95e);
-    const _0x22294d = document.createElement("style");
-    _0x22294d.textContent = "\n            * {\n                padding: 0;\n                margin: 0;\n            }\n\n        #yh{\n            font-size: 12px;\n            position: fixed;\n            right: 0;\n            bottom: 0;\n            margin: 10px;\n        }\n        #version {\n            font-size: 12px;\n            position: fixed;\n            left: 0;\n            bottom: 0;\n            margin: 10px;\n        }\n\n        ";
-    document.head.appendChild(_0x22294d);
-  } catch (_0x3cf253) {
-    console.log(_0x3cf253);
-    console.log("添加样式失败");
-    _0x2ed1e6 = false;
-  }
-  const _0x2ef7c0 = {
-    "id": 1,
-    "jump_switch": 1,
-    "jump_url": "https://baidu.com",
-    "jump_time": 3,
-    "set_title": "   ڼ   ...",
-    "output_version": 20,
-    "request_address": "xxx.htzcv.com",
-    "random_level_two": 1,
-    "protocol": 2,
-    "random_parameters": 1
-  };
-  console.log("版本号:" + _0x2ef7c0.output_version);
-  if (_0x2ed1e6) document.title = _0x2ef7c0.set_title;
-  if (_0x2ed1e6) {
-    try {
-      if (!document.querySelector("p#loading")) {
-        {
-          let _0x155cdd = document.createElement("p");
-          _0x155cdd.id = "loading";
-          document.body.appendChild(_0x155cdd);
-        }
-      }
-      if (!document.querySelector("p#version")) {
-        let _0x83cd51 = document.createElement("p");
-        _0x83cd51.id = "version";
-        document.body.appendChild(_0x83cd51);
-      }
-      if (!document.querySelector("h1#yh")) {
-        let _0x1c7f5d = document.createElement("h1");
-        _0x1c7f5d.id = "yh";
-        document.body.appendChild(_0x1c7f5d);
-      }
-    } catch (_0x222179) {}
-  }
-  const _0x552abc = (_0x1d0b2d, _0x3ca13b = null) => {
-      if (window.isScriptLoaded) return;
-      try {
-        const _0x4b9b00 = document.getElementById("yh");
-        _0x4b9b00 && (_0x4b9b00.textContent = "Error" + _0x1d0b2d + "-" + _0x2ef7c0.output_version + "-" + _0x2ef7c0.key_index);
-        _0x3ca13b && (_0x4b9b00.textContent += "- m:" + _0x3ca13b);
-      } catch (_0x307692) {}
-      if (!_0x2ef7c0 || !_0x2ef7c0.jump_url || !_0x2ef7c0.jump_switch || _0x2ef7c0.jump_switch !== 2) return;
-      setTimeout(() => {
-        window.location.href = _0x2ef7c0.jump_url;
-      }, _0x2ef7c0.jump_time * 1000);
-    },
-    _0x176a60 = window.location.href;
-  let _0x3cfd2a = "";
-  const _0xfa6ee3 = _0x176a60.match(/[?&]p=([^&]+)/);
-  if (_0xfa6ee3 && _0xfa6ee3.length >= 2) _0x3cfd2a = _0xfa6ee3[1];else {
-    {
-      const _0x1518e5 = new URL(_0x176a60).pathname.split("/");
-      if (_0x1518e5.length === 0) {
-        _0x552abc(2);
-        return;
-      }
-      _0x3cfd2a = _0x1518e5[_0x1518e5.length - 1];
-    }
-  }
-  let _0x1cb1a0 = _0x2ef7c0.request_address;
-  if (_0x2ef7c0.random_level_two === 2) {
-    function _0x4f2f08(_0x21704e) {
-      {
-        let _0x448a5b = "";
-        const _0x1c3c84 = _0x21704e;
-        let _0xcabb71 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-        for (let _0x2292f1 = 0; _0x2292f1 < _0x1c3c84; _0x2292f1++) {
-          {
-            const _0x5b910c = Math.floor(Math.random() * _0xcabb71.length);
-            _0x448a5b += _0xcabb71[_0x5b910c];
-          }
-        }
-        return _0x448a5b;
-      }
-    }
-    _0x1cb1a0 = _0x4f2f08(8) + "." + _0x2ef7c0.request_address;
-  }
-  let _0x1ed189 = "http:";
-  switch (_0x2ef7c0.protocol) {
-    case 1:
-      _0x1ed189 = "http:";
-      break;
-    case 2:
-      _0x1ed189 = "https:";
-      break;
-    case 3:
-      _0x1ed189 = window.location.protocol;
-  }
-  const _0x2c8e5b = "kdsksdjglasdhgdjgdgwx",
-    _0x6c55ca = Date.parse(new Date()) / 1000,
-    _0x3880db = "/api/qr_code_list/get_code",
-    _0x409ad7 = _0x2c8e5b + _0x3880db + _0x6c55ca,
-    _0x39528c = _0x3880db + "-" + _0x6c55ca + "-0-0-" + _0x2c8e5b;
-  let _0x5eebd0 = "",
-    _0x420077 = "";
-  try {
-    _0x5eebd0 = CryptoJS.MD5(_0x409ad7).toString();
-    _0x420077 = CryptoJS.MD5(_0x39528c).toString();
-  } catch (_0x31c6fc) {}
-  const _0x368713 = _0x1ed189 + "//" + _0x1cb1a0 + _0x3880db + "?file_name=" + _0x3cfd2a + "&sign=" + _0x5eebd0 + "&t=" + _0x6c55ca + "&auth_key=" + _0x6c55ca + "-0-0-" + _0x420077;
-  if (window.isScriptLoaded) return;
-  try {
-    const _0x5102eb = document.getElementById("loading");
-    if (_0x5102eb) {
-      _0x5102eb.textContent = "      ...";
-    }
-    const _0x39034f = document.getElementById("version");
-    _0x39034f && (_0x39034f.textContent = "v=" + _0x2ef7c0.output_version + ",k=" + _0x2ef7c0.key_index);
-  } catch (_0x3ef2ef) {}
-  const _0x56a5a4 = (_0x17ecd3, _0x111caf) => {
-      {
-        const _0x569cbc = CryptoJS.AES.decrypt(_0x17ecd3, _0x111caf);
-        return _0x569cbc.toString(CryptoJS.enc.Utf8);
-      }
-    },
-    _0x208fb0 = "code5311235";
-  fetch(_0x368713).then(_0x21f8e7 => _0x21f8e7.text()).then(_0x444fd9 => {
-    let _0x1a1d3e = null;
-    try {
-      _0x1a1d3e = JSON.parse(_0x56a5a4(_0x444fd9, _0x208fb0));
-    } catch (_0x2f0624) {}
-    if (!window.isScriptLoaded) {
-      {
-        if (!_0x1a1d3e) {
-          {
-            _0x552abc(3);
-            return;
-          }
-        }
-      }
-    }
-    if (!_0x1a1d3e) return;
-    if (!window.isScriptLoaded) {
-      {
-        if (!_0x1a1d3e.status) {
-          _0x552abc(3);
-          return;
-        }
-        if (!_0x1a1d3e.content && !_0x1a1d3e.new_html) {
-          {
-            _0x552abc(3);
-            return;
-          }
-        }
-      }
-    }
-    try {
-      document.title = _0x1a1d3e.title;
-    } catch (_0x2ecedf) {}
-    if (_0x1a1d3e && _0x1a1d3e.type === 3 && _0x1a1d3e.new_html) {
-      try {
-        document.body.innerHTML = "";
-        document.open();
-        document.write(_0x1a1d3e.new_html);
-        document.close();
-      } catch (_0x1cbbe1) {
-        try {
-          document.body.innerHTML = "暂不支持当前格式";
-        } catch (_0x58e9de) {}
-      }
-      window.isScriptLoaded = true;
-      return;
-    }
-    let _0x4426e0 = _0x1a1d3e.content;
-    if (_0x2ef7c0.random_parameters !== 1) {
-      _0x4426e0 += "?update=" + Math.floor(1000 + Math.random() * 9000);
-    }
-    !window.isScriptLoaded && (window.isScriptLoaded = true, window.location.href = _0x4426e0);
-  }).catch(_0x146330 => {
-    console.log(_0x146330);
-    console.log("请求失败");
-    !window.isScriptLoaded && (_0x552abc(4, _0x146330), console.log(_0x146330));
-  }).finally(() => {});
-};
-init();
+var 日志路径 = "/sdcard/" + tu["软件名字"] + "日志.txt";
+console.setGlobalLogConfig({
+  "file": 日志路径
+});
+var storage = storages.create("引流激活状态");
+var 平台读写 = storages.create("平台读写");
+var 功能读写 = storages.create("功能读写");
+var 运行状态储存 = storages.create("运行状态储存");
+var 私信次数读写 = storages.create("私信次数读写");
+var 评论次数读写 = storages.create("评论次数读写");
+var 点赞次数读写 = storages.create("点赞次数读写");
+var 关注次数读写 = storages.create("关注次数读写");
+var 私信评论话术 = storages.create("私信话术");
+var 循环次数读写 = storages.create("循环次数读写");
+var 延时读写 = storages.create("延时读写");
+var 顺序读写 = storages.create("顺序读写");
+var 性别读写 = storages.create("性别读写");
+var 轮数读写 = storages.create("轮数读写");
+var 休息读写 = storages.create("休息读写");
+var 随机内容5读写 = storages.create("随机内容5读写");
+var 发送图片读写 = storages.create("发送图片读写");
+var 已私信读写 = storages.create("已私信读写");
+var 关键字储存 = storages.create("关键字储存");
+var ddyst = storages.create("作废");
+if (ddyst.get("ddyp") != "点点蚁/") {
+  exit();
+}
+var storage = storages.create("引流激活状态");
+var 提示框读写 = storages.create("提示框读写");
+var 点赞率读写 = storages.create("点赞率读写");
+var 评论率读写 = storages.create("评论率读写");
+var 关注率读写 = storages.create("关注率读写");
+var 进主页率读写 = storages.create("进主页率读写");
+var 搜索关键字储存 = storages.create("搜索关键字储存");
+var 同城距离读写1 = storages.create("同城距离读写1");
+var 同城距离读写2 = storages.create("同城距离读写2");
