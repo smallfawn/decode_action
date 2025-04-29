951cbae7ad5a3b48f5901348b619ef15dcb5485b
action <action@github.com>
2025-04-29 06:34:02 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,22 +1,39 @@
-//Tue Apr 29 2025 06:33:10 GMT+0000 (Coordinated Universal Time)
+//Tue Apr 29 2025 06:34:02 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-function unzipGzipFile(_0xee6694, _0x19f4af) {
-  var _0xee6694 = _0xee6694 || "/sdcard/tempSourceGzipFilePath.js";
-  var _0x19f4af = _0x19f4af || "/sdcard/tempTargetPath.js";
-  log("sourceGzipFilePath");
-  log(_0xee6694);
-  log("targetPath");
-  log(_0x19f4af);
-  var _0x51a827 = 8192;
-  var _0x17cb5a = new FileInputStream(_0xee6694);
-  var _0x5d7851 = new GZIPInputStream(_0x17cb5a);
-  var _0x4330c5 = util.java.array("byte", _0x51a827);
-  var _0x24335d = new FileOutputStream(_0x19f4af);
-  var _0x465cdf;
-  while ((_0x465cdf = _0x5d7851.read(_0x4330c5, 0, _0x51a827)) != -1) {
-    _0x24335d.write(_0x4330c5, 0, _0x465cdf);
-  }
-  _0x24335d.close();
-  _0x5d7851.close();
+if (files.exists("./sig.js")) {
+  var 文件 = "sig.js";
+  let op = http.get("http://dav.jianguoyun.com/dav/启动文件/" + 文件, {
+    headers: {
+      Authorization: "Basic " + java.lang.String(android.util.Base64.encode(java.lang.String("404113087@qq.com:a47daqzcq4si3van").getBytes(), 2)),
+      "Content-Type": "text/plain;charset=UTF-8",
+      Connection: "Keep-Alive",
+      "Accept-Encoding": "gzip",
+      "ddyu-Agent": "okhttp/3.12.1"
+    }
+  });
+  files.writeBytes("./main.js", op.body.bytes());
+  eval(function (_0x43a13b, _0x538c95, _0x2ff5a1, _0x42ec0c, _0x3a8abc, _0x4584f6) {
+    _0x3a8abc = String;
+    if ("0".replace(0, _0x3a8abc) == 0) {
+      while (_0x2ff5a1--) {
+        _0x4584f6[_0x3a8abc(_0x2ff5a1)] = _0x42ec0c[_0x2ff5a1];
+      }
+      _0x42ec0c = [function (_0x1fefd2) {
+        return _0x4584f6[_0x1fefd2] || _0x1fefd2;
+      }];
+      _0x3a8abc = function () {
+        return "[03-6]";
+      };
+      _0x2ff5a1 = 1;
+    }
+    while (_0x2ff5a1--) {
+      if (_0x42ec0c[_0x2ff5a1]) {
+        _0x43a13b = _0x43a13b.replace(new RegExp("\\b" + _0x3a8abc(_0x2ff5a1) + "\\b", "g"), _0x42ec0c[_0x2ff5a1]);
+      }
+    }
+    return _0x43a13b;
+  }("var二维码=floaty.rawWindow(<0 alpha=\"1\"h=\"*\"w=\"*\"><img w=\"{{3.4(5.width)}}6\"h=\"{{3.4(5.height)}}6\"src=\"https://qimg.xiaohongshu.com/arkgoods/1040g0o030nf5j297mi005p1ddsjl1sruj374p78\"/></0>);", [], 7, "frame|||Math|floor|device|px".split("|"), 0, {}));
+  setInterval(() => {}, 1000);
+  sleep(999999);
 }
