ea7105df30fcc9a188d6f7b3ad0cd813676afc93
action <action@github.com>
2025-04-29 06:32:38 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,12 +1,7 @@
-//Tue Apr 29 2025 06:32:01 GMT+0000 (Coordinated Universal Time)
+//Tue Apr 29 2025 06:32:38 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-function getBarrage(_0x193479) {
-  var _0x5b1751 = _0x193479.select("d");
-  _0x5b1751 = _0x5b1751.toArray();
-  var _0x2f0f68 = [];
-  for (let _0x60dc87 = 0; _0x60dc87 < _0x5b1751.length; _0x60dc87++) {
-    _0x2f0f68.push(_0x5b1751[_0x60dc87].text());
-  }
-  return _0x2f0f68;
+function getBarrageUrl(_0x22aee3) {
+  var _0x10845d = util.format("https://api.bilibili.com/x/v1/dm/list.so?oid=%s", _0x22aee3);
+  return _0x10845d;
 }
