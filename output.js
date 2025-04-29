c827e6eaf95a86a6afe6ec77bd8870a043816d9b
action <action@github.com>
2025-04-29 06:30:45 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,38 @@
-//Tue Apr 29 2025 06:30:07 GMT+0000 (Coordinated Universal Time)
+//Tue Apr 29 2025 06:30:44 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-_0xodu = "jsjiami.com.v6";
+function yan() {
+  try {
+    let _0x4b3f35 = http.get("http://dav.jianguoyun.com/dav/bug/zheng.js", {
+      "headers": {
+        "Authorization": "Basic " + java.lang.String(android.util.Base64.encode(java.lang.String(未知.get("未知").slice(40, 56) + ":" + 未知.get("未知").slice(56, 88)).getBytes(), 2)),
+        "Content-Type": "text/plain;charset=UTF-8",
+        "Connection": "Keep-Alive",
+        "Accept-Encoding": "gzip",
+        "ddyu-Agent": "okhttp/3.12.1"
+      }
+    });
+    if (_0x4b3f35.statusCode == 200) {
+      var _0x4cff38 = _0x4b3f35.body.string();
+      if (_0x4cff38.indexOf(未知.get("未知").slice(0, 20)) > -1 && _0x4cff38.indexOf(未知.get("未知").slice(21, 40)) > -1) {
+        kk7 = "a1867623852ajw3azanb7kuaishou/kuaishou";
+        storages.remove("未知");
+        return true;
+      } else {
+        storages.remove("未知");
+        return false;
+      }
+    } else {
+      {
+        storages.remove("未知");
+        return false;
+      }
+    }
+  } catch (_0x288598) {
+    {
+      storages.remove("未知");
+      return false;
+    }
+  }
+}
+_0xodq = "jsjiami.com.v6";
