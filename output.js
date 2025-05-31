ffbe0cefbf73c8958444ff75de561433e2407bea
action <action@github.com>
2025-05-31 16:32:15 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,12 +1,9 @@
-//Sat May 31 2025 16:30:54 GMT+0000 (Coordinated Universal Time)
+//Sat May 31 2025 16:32:15 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-//Sat May 31 2025 09:31:06 GMT+0000 (Coordinated Universal Time)
-//Base:https://github.com/echo094/decode-js
-//Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("福田e家");
 const crypto = require("crypto");
-const notify = $.isNode() ? require("./sendNotify") : "";
+const notify = "";
 
 // 添加缓存相关变量
 let accountCache = {};
