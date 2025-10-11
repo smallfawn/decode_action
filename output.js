bef0c91e2b98cd46e656538a24d291aeb0229b35
action <action@github.com>
2025-10-11 07:06:59 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Tue Jun 24 2025 00:11:18 GMT+0000 (Coordinated Universal Time)
+//Sat Oct 11 2025 07:06:59 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("有道词典");
